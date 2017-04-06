d83e1f99b7f0e913bc491617f3c2c664f03c7278
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-06 12:23:36 -0700

    Configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-// var path = require('path');
-// var SRC_DIR = path.join(__dirname, '/client/src');
-// var DIST_DIR = path.join(__dirname, '/client/dist');
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/src/public');
+var DIST_DIR = path.join(__dirname, '/src/dist');
 
-// module.exports = {
-//   entry: `${SRC_DIR}/index.jsx`,
-//   output: {
-//     filename: 'bundle.js',
-//     path: DIST_DIR
-//   },
-//   module: {
-//     loaders: [{
-//       test: /\.jsx?/,
-//       include: SRC_DIR,
-//       loader: 'babel-loader',
-//       query: {
-//         presets: ['react', 'es2015', 'stage-0']
-//       }
-//     }]
-//   }
-// };
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?/,
+      include: SRC_DIR,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-0']
+      }
+    }]
+  }
+};
