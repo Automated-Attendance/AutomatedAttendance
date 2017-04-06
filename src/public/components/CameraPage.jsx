211618644ffb5991364be1ec539dd2003016811c
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-06 12:21:17 -0700

    (feat) setup webpack, made camerapage component

---
+++ src/public/components/CameraPage.jsx
@@ -0,0 +1,20 @@
+'use strict';
+
+import React from 'react';
+
+export default class CameraPage extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      Test
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<CameraPage />, document.getElementById('main'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var path = require('path');
-var SRC_DIR = path.join(__dirname, '/src/public');
-var DIST_DIR = path.join(__dirname, '/src/dist');
+var SRC_DIR = path.join(__dirname, '/src/public/components');
+var DIST_DIR = path.join(__dirname, 'src/public/dist');
 
 module.exports = {
   entry: `${SRC_DIR}/index.jsx`,
