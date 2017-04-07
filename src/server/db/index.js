bdb22927a620e8d7ca9823783781e929a758f763
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-07 12:49:41 -0700

    (feat) successfully connected to heroku clearDB mySQL server

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -1,11 +1,13 @@
 import mysql from 'mysql';
 import createTables from './config';
 import Promise from 'bluebird';
-const database = 'AutomatedAttendance';
+const database = process.env.MYSQL_DB_NAME;
 
 const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_ADMIN,
-  password: process.env.MYSQL_PASSWORD
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_NAME
 });
 
 const db = Promise.promisifyAll(connection, { multiArgs: true });
