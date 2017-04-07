9128154c2d208398ee4b7bb5cbb49337892c1fed
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-06 19:25:29 -0700

    (feat) change webpack to transpile babel stage 1

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
       include: SRC_DIR,
       loader: 'babel-loader',
       query: {
-        presets: ['react', 'es2015', 'stage-0']
+        plugins: ['transform-decorators-legacy'],
+        presets: ['react', 'es2015', 'stage-1']
       }
     }]
   }
