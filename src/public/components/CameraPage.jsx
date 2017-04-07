32d094f469119804df237537fc2056a130a52384
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-06 17:33:26 -0700

    (feat) render webcam view on camerapage component, take screenshot on button click

--- src/public/components/CameraPage.jsx
+++ src/public/components/CameraPage.jsx
@@ -2,21 +2,41 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Webcam from 'react-webcam';
+
 
 class CameraPage extends React.Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      screenshot: null
+    };
+
+    this.takeScreenshot = this.takeScreenshot.bind(this);
   }
 
-  componentDidMount() {
-    const camera = new JpegCamera('#camera');
+  takeScreenshot() {
+    const screenshot = this.refs.webcam.getScreenshot();
+    this.setState({ screenshot: screenshot });
   }
 
   render() {
     return (
       <div>
-        <div id="camera" />
+
+        <Webcam
+          ref='webcam'
+        />;
+
+        <h1> Screenshots </h1>
+
+        <div className="screenshots">
+          <button onClick={this.takeScreenshot}>Take Screenshot</button>
+          { this.state.screenshot ? <img src={this.state.screenshot} /> : null }
+        </div>
+
       </div>
     );
   }
