f8a98d7f900e3a6514000c1c99cf45c9deb10e31
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-08 12:09:40 -0700

    (cleanup) fixed styling on login redirect

--- src/public/components/LoginRedirect.jsx
+++ src/public/components/LoginRedirect.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const LoginRedirect = () => {
   return (
     <div>
-    <h1>You must login to view this page</h1>
-    <a href="/login"><button>Login</button></a>
+      <h1>You must login to view this page</h1>
+      <a href="/login"><button>Login</button></a>
     </div>
   );
 };
