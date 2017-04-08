901f36f00b1151c05ab26e227744da95e42c7d22
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-08 14:42:05 -0700

    Update search.js
    removed console.log

--- src/server/db/search.js
+++ src/server/db/search.js
@@ -14,7 +14,6 @@ exports.searchDB = function(req, res) {
 
 
 exports.getAttendanceForUser = function(req, res) {
-  console.log('REQQQQQQQ', req)
   var queryString = "select users.name,status,date,AttendanceRecord.id from users right join AttendanceRecord on users.id=AttendanceRecord.user_id;";
   db.query(queryString, function(error, result) {
     if (error) {
