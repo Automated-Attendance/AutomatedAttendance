8ecd97a9b83b0c7dedee32afe943e40552cefc34
Han <hanshengzhao1993@gmail.com>
2017-04-08 14:28:44 -0700

    Pulling information from Database in users

--- src/public/components/Student.jsx
+++ src/public/components/Student.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import autoBind from 'react-autobind';
+import { post, get } from './AxiosRoutes.js';
 
 
 export default class Student extends React.Component {
@@ -9,49 +10,33 @@ export default class Student extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      fakeData: [{
-        id: 1,
-        name: 'han',
-        date: '4/1',
-        status: 'Ontime'
-      }, {
-        id: 2,
-        name: 'han',
-        date: '4/2',
-        status: 'Ontime'
-      }, {
-        id: 3,
-        name: 'han',
-        date: '4/3',
-        status: 'Ontime'
-      }, {
-        id: 4,
-        name: 'han',
-        date: '4/4',
-        status: 'Ontime'
-      }, {
-        id: 5,
-        name: 'han',
-        date: '4/5',
-        status: 'Ontime'
-      }]
+      fakeData: []
     };
     
     autoBind(this);
   }
 
   componentDidMount () {
+    post('studentInformation', {name: 'Han'})
+      .then( (response) => {
+        this.setState({fakeData: response.data})
+      })
+      .catch( (err) => {
+        console.log(err);
+      });
+
     // make a post request about specific user
   }
   
   mapFakeData() {
     const data = this.state.fakeData;
-    const nameList = data.map(name => {
+    const nameList = data.map( name => {
       return (
         <tr key={name.id}>
           <td>{name.name}</td>
           <td>{name.date}</td>
           <td>{name.status}</td>
+      
         </tr>
       );
     });
