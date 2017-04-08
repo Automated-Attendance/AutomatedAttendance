cfbaa92694c9c1e75db7ad02b044555407519fed
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-07 19:06:08 -0700

    Update search.js

--- src/server/db/search.js
+++ src/server/db/search.js
@@ -2,14 +2,12 @@ import mysql from 'mysql';
 import db from './index.js';
 
 exports.searchDB = function(req, res) {
-  console.log('searchDB');
   var queryString = 'SELECT * FROM users;';
   db.query(queryString, function(error, result) {
     if (error) {
-      console.log('error searching database');
+      res.status(500).send(error);
+    } else {
+      res.json(result);
     }
-    console.log('success');
-    console.log(result);
-    res.json(result);
   });
 }
