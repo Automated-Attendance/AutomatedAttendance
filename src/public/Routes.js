484ef2cba8909b4f5047665d2a26940978383fce
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-09 01:21:17 -0700

    (feat) fixed spacing for easier testing

--- src/public/Routes.js
+++ src/public/Routes.js
@@ -10,14 +10,14 @@ import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom
 
 const Routes = ({ userPrivs }) => {
   return (
-     <div>
-        <Route path="/About" component={About}/>
-        <Route path="/Contact" component={Contact}/>
-        <Route path="/Student" component={() => userPrivs.isLoggedIn && !userPrivs.isAdmin ? <Student/> : <LoginRedirect userPrivs={userPrivs}/> }/>
-        <Route path="/CameraPage" component={() => userPrivs.isAdmin ? <CameraPage/> : <LoginRedirect userPrivs={userPrivs}/> }/>
-        <Route path="/Admin" component={() => userPrivs.isAdmin ? <Admin/> : <LoginRedirect userPrivs={userPrivs}/> }/>
-        <Route path="/AddStudent" component={() => userPrivs.isAdmin ? <AddStudent/> : <LoginRedirect userPrivs={userPrivs}/> }/>
-      </div>
+    <div>
+      <Route path="/About" component={About}/>
+      <Route path="/Contact" component={Contact}/>
+      <Route path="/Student" component={() => userPrivs.isLoggedIn && !userPrivs.isAdmin ? <Student/> : <LoginRedirect userPrivs={userPrivs}/> }/>
+      <Route path="/CameraPage" component={() => userPrivs.isAdmin ? <CameraPage/> : <LoginRedirect userPrivs={userPrivs}/> }/>
+      <Route path="/Admin" component={() => userPrivs.isAdmin ? <Admin/> : <LoginRedirect userPrivs={userPrivs}/> }/>
+      <Route path="/AddStudent" component={() => userPrivs.isAdmin ? <AddStudent/> : <LoginRedirect userPrivs={userPrivs}/> }/>
+    </div>
   );
 };
 
