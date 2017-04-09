6a4d1fde8d4d349305329d1eb4c03e9e09069846
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-09 15:14:10 -0700

    (feat) add about-test.js file

---
+++ test/About-test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow, mount, render } from 'enzyme';
+import { spy } from 'sinon';
+import { expect } from 'chai';
+import { Link } from 'react-router-dom';
+import App from '../src/public/components/Navigation';
+
+
+describe('<About />', function() {
+
+
+
+});
