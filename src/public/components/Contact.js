ee2a2fb3b75afa406589609fd9214a8a2f3dcac4
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-09 16:41:37 -0700

    (refactor) changed Contact component to functional stateless

--- src/public/components/Contact.js
+++ src/public/components/Contact.js
@@ -1,24 +1,17 @@
-'use strict';
-
 import React from 'react';
 
-export default class Contact extends React.Component {
+const Contact = () => {
+  return (
+    <div>
+      <h3 className="header">
+        Questions or comments?
+      </h3>
+      <h5>
+        Contact us:
+      </h5>
+      <a href="mailto:AAAllstars@gmail.com?Subject=Automated%20Attendance%20Support">AAAllstars@gmail.com</a>
+    </div>
+  );
+};
 
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div>
-        <h3 className="header">
-          Questions or comments?
-        </h3>
-        <h5>
-          Contact us:
-        </h5>
-        <a href="mailto:AAAllstars@gmail.com?Subject=Automated%20Attendance%20Support">AAAllstars@gmail.com</a>
-      </div>
-    );
-  }
-}
+export default Contact;
