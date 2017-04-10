3605fce44e1b93725ed7e2355a414f99ae316e70
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-09 17:26:08 -0700

    (feat) finished LoginRedirect component tests

---
+++ test/LoginRedirect-test.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import { Link } from 'react-router-dom';
+import LoginRedirect from '../src/public/components/LoginRedirect';
+
+
+describe('<LoginRedirect />', function() {
+
+  const loggedIn = {
+    isLoggedIn: true,
+    isAdmin: true
+  };
+
+  const notLoggedIn = {
+    isLoggedIn: false,
+    isAdmin: false
+  };
+
+  it('should receive props', () => {
+    const wrapper = shallow(<LoginRedirect userPrivs={loggedIn}/>);
+    const userPrivs = wrapper.instance().props.userPrivs;
+    expect(userPrivs).to.exist;
+    expect(userPrivs.isLoggedIn).to.exist;
+    expect(userPrivs.isAdmin).to.exist;
+  });
+
+  it('should render forbidden access Link if logged in and not adequate privileges', () => {
+    const wrapper = shallow(<LoginRedirect userPrivs={loggedIn}/>);
+    expect(wrapper.contains(
+      <Link to="/">You don't have correct access to view this page. 
+        <button>Click to go home.</button>
+      </Link>)).to.equal(true);
+  });
+
+  it('should not render forbidden access Link if not logged in', () => {
+    const wrapper = shallow(<LoginRedirect userPrivs={notLoggedIn}/>);
+    expect(wrapper.contains(
+      <Link to="/">You don't have correct access to view this page. 
+        <button>Click to go home.</button>
+      </Link>)).to.equal(false);
+  });
+
+  it('should render login Link if not logged in', () => {
+    const wrapper = shallow(<LoginRedirect userPrivs={notLoggedIn}/>);
+    expect(wrapper.contains(<a href="/login"><button>Login</button></a>)).to.equal(true);
+  });
+
+  it('should not render login Link if logged in', () => {
+    const wrapper = shallow(<LoginRedirect userPrivs={loggedIn}/>);
+    expect(wrapper.contains(<a href="/login"><button>Login</button></a>)).to.equal(false);
+  });
+
+});
