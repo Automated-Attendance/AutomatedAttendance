ab16dcd7e57ea221a82eed2d0369c0073ffb3a8f
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-09 20:23:22 -0700

    (feat) added 5 CameraPage tests

--- test/CameraPage-test.js
+++ test/CameraPage-test.js
@@ -1,13 +1,39 @@
 import React from 'react';
 import keydown, { Keys } from 'react-keydown';
-import { shallow } from 'enzyme';
+import { shallow, mount, render } from 'enzyme';
 import { expect } from 'chai';
+import { spy } from 'sinon';
 import CameraPage from '../src/public/components/CameraPage';
 
 describe('<CameraPage />', function() {
 
+
   it('should render with a webcam component view', () => {
     const wrapper = shallow(<CameraPage/>);
+    expect(wrapper.find('Webcam')).to.have.length(1);
+  });
+
+  it('should call takeScreenshot on click', () => {
+    const testFn = spy(CameraPage.prototype, 'takeScreenshot');
+    const wrapper = mount(<CameraPage />);
+    expect(testFn.called).to.equal(false);
+    wrapper.find('button').simulate('click');
+    expect(testFn.called).to.equal(true);
+    testFn.restore();
+  });
+
+  it('should call uploadToCloudinary on click', () => {
+    const testFn = spy(CameraPage.prototype, 'uploadToCloudinary');
+    const wrapper = mount(<CameraPage />);
+    expect(testFn.called).to.equal(false);
+    wrapper.find('button').simulate('click');
+    expect(testFn.called).to.equal(true);
+    testFn.restore();
+  });
+
+  it('should not have img node when no screenshot has been stored', () => {
+    const wrapper = shallow(<CameraPage />);
+    expect(wrapper.find('img')).to.have.length(0);
   });
 
 });
