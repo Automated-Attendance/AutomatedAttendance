572cdd0678a8888461c260a89f11f47d06445abe
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-10 11:33:52 -0700

    Parse date into separate date and time columns

--- src/public/components/Admin.js
+++ src/public/components/Admin.js
@@ -15,12 +15,26 @@ export default class Admin extends React.Component {
     const queryType = {queryType: 'allAttendance'};
     const attendanceRecords = await getAttendanceRecords(queryType);
     this.setState({ attendance: attendanceRecords });
-  }
-
-  handleClassListChange(currentClass){
-    this.setState({
-      selectedClass: currentClass
-    })
+    // try {
+    //   const { data } = await post('search', {queryType: 'allAttendance'})
+    //   data.forEach((item) => {
+    //     let timeStartIndex = item.date.indexOf('T');
+    //     let hour = item.date.slice(++timeStartIndex, timeStartIndex + 2) - 7;
+    //     let suffix = 'AM';
+    //     if (hour > 12) {
+    //       hour -= 12;
+    //       suffix = 'PM';
+    //     } else if (hour < 1) {
+    //       hour += 12;
+    //       suffix = 'PM';
+    //     }
+    //     item.time = `${hour}${item.date.slice(timeStartIndex + 2, timeStartIndex + 8)} ${suffix}`;
+    //     item.date = item.date.slice(0, 10);
+    //   })
+    //   this.setState({attendance: data});
+    // } catch (err) {
+    //   console.log(err);
+    // }
   }
 
   render() {
@@ -31,6 +45,7 @@ export default class Admin extends React.Component {
           <TableHeaderColumn dataField='class_name' isKey filter={{type: 'TextFilter'}} dataSort={true}>Class</TableHeaderColumn>
           <TableHeaderColumn dataField='user_name' filter={{type: 'TextFilter'}} dataSort={true}>Student</TableHeaderColumn>
           <TableHeaderColumn dataField='date' filter={{type: 'TextFilter'}} dataSort={true}>Date</TableHeaderColumn>
+          <TableHeaderColumn dataField='time' dataSort={true}>Time</TableHeaderColumn>
           <TableHeaderColumn dataField='status' filter={{type: 'TextFilter'}} dataSort={true}>Status</TableHeaderColumn>
         </BootstrapTable>
 
