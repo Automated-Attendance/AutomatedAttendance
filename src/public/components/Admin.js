1f236aa01844d9693c84c5f0c1f9806804dc779a
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-10 12:00:05 -0700

    Format date to output month in text and removing leading zeros from days

--- src/public/components/Admin.js
+++ src/public/components/Admin.js
@@ -27,6 +27,7 @@ export default class Admin extends React.Component {
         }
         item.time = `${hour}${item.date.slice(timeStartIndex + 2, timeStartIndex + 8)} ${suffix}`;
         item.date = item.date.slice(0, 10);
+        item = this.parseDateAndTime(item);
       })
       this.setState({attendance: data});
     } catch (err) {
@@ -42,20 +43,51 @@ export default class Admin extends React.Component {
     if (hour > 12) {
       hour -= 12;
       suffix = 'PM';
-    } else if (hour < 1) {
+    } else if (hour <= 0) {
       hour += 12;
       suffix = 'PM';
     }
     if (hour === 12) {
-      if (suffix ===  'AM') {
-        suffix = 'PM';
-      } else {
-        suffix = 'AM';
-      }
+      suffix = suffix === 'AM' ? 'PM' : 'AM';
     }
     result.time = `${hour}${result.date.slice(timeStartIndex + 2, timeStartIndex + 8)} ${suffix}`;
-    result.date = result.date.slice(0, 10);
+    result.date = this.convertDate(result.date.slice(0, 10));
     return result;
+  }
+
+  convertDate(date) {
+    var year = date.slice(0, 4);
+    var month = date.slice(5, 7);
+    var day = date.slice(8);
+    if (month === '01') {
+      month = 'January';
+    } else if (month === '02') {
+      month = 'February';
+    } else if (month === '03') {
+      month = 'March';
+    } else if (month === '04') {
+      month = 'April';
+    } else if (month === '05') {
+      month = 'May';
+    } else if (month === '06') {
+      month = 'June';
+    } else if (month === '07') {
+      month = 'July';
+    } else if (month === '08') {
+      month = 'August';
+    } else if (month === '09') {
+      month = 'September';
+    } else if (month === '10') {
+      month = 'October';
+    } else if (month === '11') {
+      month = 'November';
+    } else if (month === '12') {
+      month = 'December';
+    }
+    if (day.charAt(0) === '0') {
+      day = day.slice(1);
+    }
+    return `${month} ${day}, ${year}`;
   }
 
   render() {
