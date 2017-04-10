760da4bfccd044cd3e521b5f643c995400fba914
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-10 14:31:03 -0700

    (refactor) move isLoggedIn functionality into users.js in requests

--- src/public/components/index.js
+++ src/public/components/index.js
@@ -2,35 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import Navigation from './Navigation';
-import { get } from './AxiosRoutes';
+import { getUserData } from './requests/users';
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
       isLoggedIn: false,
       isAdmin: false
     };
-    
-    this.isLoggedIn = this.isLoggedIn.bind(this);
   }
 
-  componentWillMount() {
-    this.isLoggedIn();
-  }
-
-  async isLoggedIn() {
-    try {
-      const { data } = await get('userData');
-      const loggedIn = data !== 'not logged in';
-      if (loggedIn && data[0].type === 'admin') this.setState({ isAdmin: true });
-      if (loggedIn) this.setState({ isLoggedIn: true });
-    } catch (err) {
-      // todo: handle client side errors better
-      console.warn(err);
-    }
+  async componentWillMount() {
+    const loginStatus = await getUserData();
+    this.setState(loginStatus);
   }
 
   render () {
