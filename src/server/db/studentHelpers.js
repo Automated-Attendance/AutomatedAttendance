d7cd4fb914261f375bbe606ee3af7ad700d5c288
Duy Nguyen <nguyenaiden@gmail.com>
2017-04-10 16:06:11 -0700

    Fixed syntax and spacing

--- src/server/db/studentHelpers.js
+++ src/server/db/studentHelpers.js
@@ -1,9 +1,7 @@
 import db from './index';
 
-
-
 exports.addStudent = (req, res) => {
-  let addUser = `INSERT INTO users (user_name,email,photo) VALUES ('${req.body.studentName}','${req.body.studentEmail}','${req.body.link}')`;
+  let addUser = `INSERT INTO users (user_name, email, photo) VALUES ('${req.body.studentName}','${req.body.studentEmail}','${req.body.link}')`;
   let addUserClass = `INSERT INTO class_user (class_id, user_id) SELECT classes.classes_id, users.users_id FROM classes, users WHERE users.email='${req.body.studentEmail}' AND classes.class_name='${req.body.selectedClass}'`;
   db.queryAsync(addUser, (error, result) => {
     if (error) {
@@ -15,7 +13,7 @@ exports.addStudent = (req, res) => {
         } else {
           res.status(201).send('Upload Sucessful!');
         }
-      })
+      });
     }
-  })
+  });
 }
