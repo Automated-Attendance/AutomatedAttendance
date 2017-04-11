02a9fe36784dfe90d71a13b9fba3c29807c87e20
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-11 11:26:12 -0700

    (refactor) cleaned up studentHelper addStudent method

--- src/server/db/studentHelpers.js
+++ src/server/db/studentHelpers.js
@@ -1,19 +1,37 @@
 import db from './index';
 
-exports.addStudent = (req, res) => {
-  let addUser = `INSERT INTO users (user_name, email, photo) VALUES ('${req.body.studentName}', '${req.body.studentEmail}', '${req.body.link}')`;
-  let addUserClass = `INSERT INTO class_user (class_id, user_id) SELECT classes.classes_id, users.users_id FROM classes, users WHERE users.email='${req.body.studentEmail}' AND classes.class_name='${req.body.selectedClass}'`;
-  db.queryAsync(addUser, (error, result) => {
-    if (error) {
-      res.status(500).send(error);
-    } else {
-      db.queryAsync(addUserClass, (error, result) => {
-        if (error) {
-          res.status(500).send(error);
-        } else {
-          res.status(201).send('Upload Sucessful!');
-        }
-      });
-    }
-  });
+exports.addStudent =  async (req, res) => {
+  try {
+    const { studentName, studentEmail, selectedClass, link } = req.body
+
+    let addUser = `INSERT INTO users (user_name, email, photo) 
+    VALUES ('${studentName}', '${studentEmail}', '${link}')`;
+
+    let addUserClass = `INSERT INTO class_user (class_id, user_id) 
+    SELECT classes.classes_id, users.users_id FROM classes, users 
+    WHERE users.email='${studentEmail}' 
+    AND classes.class_name='${selectedClass}'`;
+
+    await db.queryAsync(addUser);
+    await db.queryAsync(addUserClass);
+    res.status(201).send('Upload Sucessful!');
+
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
+
+// old with callbacks for reference
+// db.queryAsync(addUser, (error, result) => {
+//   if (error) {
+//     res.status(500).send(error);
+//   } else {
+//     db.queryAsync(addUserClass, (error, result) => {
+//       if (error) {
+//         res.status(500).send(error);
+//       } else {
+//         res.status(201).send('Upload Sucessful!');
+//       }
+//     });
+//   }
+// });
