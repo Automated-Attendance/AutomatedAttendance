661c78a2116d5ec21c26c5b637027429419e043b
Han <hanshengzhao1993@gmail.com>
2017-04-11 15:23:55 -0700

    Added New Request handlers

--- src/server/mailgun/mailGunHelpers.js
+++ src/server/mailgun/mailGunHelpers.js
@@ -1,18 +1,27 @@
-// const api_key = process.env.MAILGUN_API_KEY;
-// const domain = process.env.MAILGUN_DOMAIN;
-// const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+import MailGun from 'mailgun-js'
+const mailgun = MailGun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
  
-// var data = {
-//   from: 'Excited User <aaallstars15@gmail.com>',
-//   to: 'hanshengzhao1993@gmail.com',
-//   subject: 'Hello',
-//   text: 'Testing some Mailgun awesomness!'
-// };
- 
-// mailgun.messages().send(data, function (error, body) {
-//   if(error){
-//     console.log(error)
-//   } else {
-//     console.log(body);
-//   }
-// });
+exports.sendMail = (req, res) => {
+  // console.log(req.params);
+  req.params.forEach( (person)=> {
+    console.log('email', person.email);
+    var data = { 
+      from: 'Excited User <aaallstars15@gmail.com>',
+      to: person.email,
+      subject: 'Late Email Warning',
+      text: '1 minutes till class starts! Email communication@ if you are going to be late.'
+    };
+    
+    mailgun.messages().send(data, function (error, body) {
+      if (error) {
+        res.status(500).send(error)
+        console.log(error);
+      } else {
+        console.log(body);
+      }
+    })
+  });
+};
+  
+
+
