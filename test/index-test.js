1fca623b13126c54b5ca3c016a50082caf9d809c
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-10 22:28:17 -0700

    (feat) finish tests for index component

--- test/index-test.js
+++ test/index-test.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import { shallow, mount, render } from 'enzyme';
-import { spy } from 'sinon';
+import sinon from 'sinon';
 import { expect } from 'chai';
+import axios from 'axios';
+import { getUserData } from '../src/public/components/requests/users';
 import App from '../src/public/components/index';
 
 describe('<App />', function() {
+
+  let sandbox;
+  beforeEach(() => {
+    sandbox = sinon.sandbox.create();
+    const resolved = new Promise((res) => res({ data: [{ type: 'admin' }] }));
+    sandbox.stub(axios, 'get').returns(resolved);
+  });
+
+  afterEach(() => {
+    sandbox.restore();
+  });
   
-  it('calls componentWillMount', () => {
-    const componentWillMountSpy = spy(App.prototype, 'componentWillMount');
+  it('calls componentWillMount', function() {
+    const componentWillMountSpy = sinon.spy(App.prototype, 'componentWillMount');
     const wrapper = shallow(<App />);
     expect(App.prototype.componentWillMount.calledOnce).to.equal(true);
     componentWillMountSpy.restore();
   });
 
-  it('contains a Navigation bar', () => {
+  it('contains a Navigation bar', function() {
     const wrapper = mount(<App/>);
     expect(wrapper.find('Navigation')).to.have.length(1);
   });
 
-  it('contains a Routes child component', () => {
+  it('contains a Routes child component', function() {
     const wrapper = mount(<App/>);
     expect(wrapper.find('Routes')).to.have.length(1);
   });
 
-  it('should receive a response from server after login', () => {
-
+  it('should receive a response from server after login', async function() {
+    try {
+      const wrapper = mount(<App/>);
+      expect(wrapper.state().isLoggedIn).to.equal(true);
+      expect(wrapper.state().isAdmin).to.equal(true);
+    } catch (err) {
+      console.warn(err);
+    }
   });
 
 });
