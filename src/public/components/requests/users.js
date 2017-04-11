44586b0960877ef4ebba2554154aae4fe4521365
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-10 19:16:18 -0700

    (feat) attach user email to loginStatus object

--- src/public/components/requests/users.js
+++ src/public/components/requests/users.js
@@ -5,14 +5,14 @@ const getUserData = async function() {
     const { data } = await axios.get('/retrieveUserData');
     const loggedIn = data !== 'not logged in';
     const admin = data[0].type === 'admin';
-    let status = { isLoggedIn: false, isAdmin: false };
-    
+    let status = { isLoggedIn: false, isAdmin: false, userEmail: null };
     if (loggedIn && admin) {
       status.isAdmin = true;
     }
 
     if (loggedIn) {
       status.isLoggedIn = true;
+      status.userEmail = data[0].email;
     }
 
     return status;
