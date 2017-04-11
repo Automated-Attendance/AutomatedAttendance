7d34e4b012bb039a1b5287fee71443d6c09a8a32
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-11 14:01:06 -0700

    Update classHelpers.js
    spacing

--- src/server/db/classHelpers.js
+++ src/server/db/classHelpers.js
@@ -4,7 +4,7 @@ exports.getClass = async (req, res) => {
   try {
     let queryClass = 'SELECT class_name FROM classes';
     const classes = await db.queryAsync(queryClass);
-     res.status(200).send(classes);
+    res.status(200).send(classes);
   }
   catch (err) {
     res.status(500).send(err);
