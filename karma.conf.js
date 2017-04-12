533bfca463841d14d2da1830f837e682e2650312
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-12 16:42:32 -0700

    (feat) added dependencies for datetime picker

--- karma.conf.js
+++ karma.conf.js
@@ -28,10 +28,10 @@ module.exports = function(config) {
               presets: ['react', 'es2015', 'stage-1']
             }
           },
-          { 
-            test: /\.css$/, 
-            loader: 'style-loader!css-loader'
-          }
+          { test: /\.css$/, loader: 'style-loader!css-loader' },
+          { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+          { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+          { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=[name].[ext]' }
         ]
       },
       externals: {
