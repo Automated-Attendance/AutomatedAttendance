3eece028b581d0a56dd5023adefe1bf3082ef2a8
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-12 15:44:30 -0700

    Update insert.js

--- src/server/db/insert.js
+++ src/server/db/insert.js
@@ -6,16 +6,12 @@ Promise.promisifyAll(db);
 
 exports.insertAttendanceRecord = async (req, res) => {
   try {
-    console.log('REQ>BODY',req.body.params[0].length);
-
-    req.body.params[0].forEach(function (user) {
+    req.body.params[0].forEach((user) => {
       let id =user.user_id;
       let status = 'Absent';
       let add = `INSERT INTO attendance_record(status, user_id) VALUES ('${status}', '${id}');`
-
       db.queryAsync(add);
-    })
-
+    });
     res.status(201).send();
   } catch (err) {
     console.warn(err);
