aebf04b0da2c2e4f947232d48c2fb98a0b70c9d4
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-11 21:54:11 -0700

    (refactor) userHelpers from promises to await

--- src/server/db/userHelpers.js
+++ src/server/db/userHelpers.js
@@ -3,42 +3,45 @@ import db from './index';
 
 Promise.promisifyAll(db);
 
-exports.storeIfNew = (req, res, next) => {
-  let profile = req.user;
-  db.queryAsync(`SELECT email FROM users WHERE email='${profile.emails[0].value}'`)
-  .then((results) => {
+exports.storeIfNew = async (req, res, next) => {
+  try {
+    let profile = req.user;
+    const results = await db.queryAsync(`SELECT email FROM users WHERE email='${profile.emails[0].value}'`);
     if (!results[0].length) {
       let newUser = { user_name: profile.nickname, email: profile.emails[0].value };
-      db.queryAsync('INSERT INTO users SET ?', newUser)
-      .then(() => next())
-      .catch((err) => res.status(500).send(err));
-    } else {
-      next();
+      await db.queryAsync('INSERT INTO users SET ?', newUser)
     }
-  })
-  .catch((err) => res.status(500).send(err));
-};
-
-exports.retrieveData = (req, res) => {
-  if (req.user) {
-    const userEmail = req.user.emails[0].value;
-    const qs = `SELECT * FROM users WHERE email='${userEmail}'`;
-    db.queryAsync(qs)
-    .then((response) => res.send(response[0]));
-  } else {
-    res.send('not logged in');
+    next(); 
+  } catch (err) {
+    res.status(500).send(err);
   }
 };
 
-exports.updateIfAdmin = (req, res, next) => {
-  const adminPrivs = req.user._json.role === 'admin';
-  const userEmail = req.user.emails[0].value;
-  if (adminPrivs) {
-    const qs = `UPDATE users SET type='admin' WHERE email='${userEmail}'`;
-    db.queryAsync(qs)
-    .then((response) => next())
-    .catch((err) => res.status(500).send(err));
-  } else {
+exports.retrieveData = async (req, res) => {
+  try {
+    if (req.user) {
+      const userEmail = req.user.emails[0].value;
+      const selectUser = `SELECT * FROM users WHERE email='${userEmail}'`;
+      const response = await db.queryAsync(selectUser)
+      res.send(response[0]);
+    } else {
+      res.send('not logged in');
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+exports.updateIfAdmin = async (req, res, next) => {
+  try {
+    const adminPrivs = req.user._json.role === 'admin';
+    const userEmail = req.user.emails[0].value;
+    if (adminPrivs) {
+      const updateToAdmin = `UPDATE users SET type='admin' WHERE email='${userEmail}'`;
+      await db.queryAsync(updateToAdmin);
+    }
     next();
+  } catch (err) {
+    res.status(500).send(err);
   }
 };
