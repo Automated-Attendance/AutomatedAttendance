748023a9e1a64f87a98437fe96d89e196b954496
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-12 22:40:47 -0700

    (feat) condensed upload / post into one function with conditional

--- src/server/cloudinary/cloudHelpers.js
+++ src/server/cloudinary/cloudHelpers.js
@@ -3,23 +3,17 @@ import Promise from 'bluebird';
 
 Promise.promisifyAll(cloudinary);
 
-exports.post = async (req, res) => {
-  try {
-    const { img } = req.body;
-    const options = { format: 'png', public_id: 'temporary' };
-    const result = await cloudinary.v2.uploader.upload(img, options);
-    res.send(result);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-};
-
 exports.upload = async (req, res, next) => {
   try {
-    const { studentPhoto, studentUserName } = req.body;
-    const options = { format: 'png', 'public_id': studentUserName }
-    const { url } = await cloudinary.v2.uploader.upload(studentPhoto, options);
+    let { studentPhoto, studentUserName, img } = req.body;
+    let options = { format: 'png', 'public_id': 'temporary' }
+    if (!img) {
+      options.public_id = studentUserName;
+      img = studentPhoto;
+    }
+    const { url } = await cloudinary.v2.uploader.upload(img, options);
     req.body.imageLink = url;
+    delete req.body.img;
     next();
   } catch (err) {
     res.status(500).send(err.message);
