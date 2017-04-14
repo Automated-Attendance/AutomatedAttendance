bc364a82182013c1f40f7a908d64259a0b4dc9f9
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-14 00:50:25 -0700

    (refactor) removed unused fn

--- src/server/db/insert.js
+++ src/server/db/insert.js
@@ -4,27 +4,6 @@ import db from './index.js';
 
 Promise.promisifyAll(db);
 
-exports.insertAttendanceRecord = async (req, res) => {
-  try {
-    let users = req.body.params[0];
-    users.forEach(async (user) => {
-      let result = await db.queryAsync(`SELECT date FROM attendance_record WHERE user_id='${user.user_id}'`);
-      if (result[0].length) {
-        let existingDay = new Date(result[0][0].date).getDay();
-        let currentDay = new Date().getDay();
-        if (existingDay !== currentDay) {
-          db.queryAsync(`INSERT INTO attendance_record(status, user_id) VALUES ('Pending', '${user.user_id}');`);
-        }
-      } else {
-        db.queryAsync(`INSERT INTO attendance_record(status, user_id) VALUES ('Pending', '${user.user_id}');`);
-      }
-    });
-    res.status(201).send();
-  } catch (err) {
-    console.warn(err);
-  }
-};
-
 exports.insertAbsentRecord = async (req, res, next) => {
   try {
     // results[].forEach(function (element) {})
