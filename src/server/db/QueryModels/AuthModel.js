c3957f7173e5ebc1725a2a914b65040a6b644518
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-14 16:10:35 -0700

    (feat) added AuthModel and AuthQueries classes

---
+++ src/server/db/QueryModels/AuthModel.js
@@ -0,0 +1,33 @@
+import UserQueries from '../QuerySelectors/AuthQueries';
+import Promise from 'bluebird';
+import db from '../index';
+
+Promise.promisifyAll(db);
+
+
+export default class UserModel extends UserQueries {
+
+  constructor() {
+    super();
+  }
+
+  async storeIfNew({ email, name, nickname }) {
+    let [ firstName, lastName ] = name.split(' ');
+    let userCheckQuery = super.selectExistingUser(email);
+    let [userInfo] = await db.queryAsync(userCheckQuery);
+    if (!userInfo.length) {
+      let insertUserQuery = super.insertNewUser(firstName, lastName, nickname, email);
+      await db.queryAsync(insertUserQuery);
+    }
+  }
+
+  async updateIfAdmin({ role, email }) {
+    let updateQuery = super.updateToAdmin(email);
+    if (role === 'admin') {
+      await db.queryAsync(updateQuery);
+      return 'admin';
+    }
+    return undefined;
+  }
+
+};
