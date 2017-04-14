872c96fc78cdce42bdad9528fca03ab45a99eb1c
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-13 21:40:38 -0700

    (feat) created QuerySelector class for centralizing database queries

---
+++ src/server/db/Queries/QueryModel.js
@@ -0,0 +1,17 @@
+
+
+// class QueryModel {
+
+//   async selectNameAndGithub() {
+//     try {
+//       const query = `SELECT first_name, last_name, user_name FROM users`;
+//       const result = await db.queryAsync(query);
+//       return result[0];
+//     } catch (err) {
+//       res.status(500).send(err.message);
+//     }
+//   }
+
+  
+
+// }
