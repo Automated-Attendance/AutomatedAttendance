6d17ea563a23652e0b986ce1f3c5d2de2655f702
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-14 16:47:16 -0700

    (feat) fixed upload in cloudinary

--- src/server/cloudinary/cloudHelpers.js
+++ src/server/cloudinary/cloudHelpers.js
@@ -3,19 +3,11 @@ import Promise from 'bluebird';
 
 Promise.promisifyAll(cloudinary);
 
-exports.upload = async (req, res, next) => {
-  try {
-    let { studentPhoto, studentUserName, img } = req.body;
-    let options = { format: 'png', 'public_id': 'temporary' }
-    if (!img) {
-      options.public_id = studentUserName;
-      img = studentPhoto;
-    }
-    const { url } = await cloudinary.v2.uploader.upload(img, options);
-    req.body.imageLink = url;
-    delete req.body.img;
-    next();
-  } catch (err) {
-    res.status(500).send(err.message);
+exports.upload = async ({ studentPhoto, studentUserName, img }) => {
+  let options = { format: 'png', 'public_id': 'temporary' }
+  if (!img) {
+    options.public_id = studentUserName;
+    img = studentPhoto;
   }
+  return await cloudinary.v2.uploader.upload(img, options);
 };
