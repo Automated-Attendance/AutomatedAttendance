d81c62324d90dea2d542261aab6a19a5f68258d2
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-14 02:31:49 -0700

    (feat) moved middleware to attendance helpers

--- src/server/db/attendanceHelpers.js
+++ src/server/db/attendanceHelpers.js
@@ -11,3 +11,26 @@ exports.storeRecords = async (req, res) => {
     res.status(500).send(err.message);
   }
 }
+
+exports.getRecords = async (req, res) => {
+  try {
+    let result, { type, email } = req.query;
+    if (type === 'allAttendance') {
+      result = await Attendance.getAllRecords();
+    } else {
+      result = await Attendance.getStudentRecord(email);
+    }
+    res.json(result[0]);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
+
+exports.emailLateStudents = async (req, res) => {
+  try {
+    await Attendance.emailLateStudents();
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+}
