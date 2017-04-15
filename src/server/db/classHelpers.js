f4f36dc6280bc5d399264245a6f0cd20592aa7a0
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-14 17:17:09 -0700

    Remove console.logs

--- src/server/db/classHelpers.js
+++ src/server/db/classHelpers.js
@@ -29,7 +29,6 @@ exports.getEnrollment = async (req, res) => {
       RIGHT JOIN class_user ON classes.classes_id=class_user.class_id
       LEFT JOIN users ON class_user.user_id=users.users_id`;
     const enrollment = await db.queryAsync(enrollmentQuery)
-    console.log('enrollment', enrollment);
     res.status(201).send(enrollment);
   }
   catch (err) {
