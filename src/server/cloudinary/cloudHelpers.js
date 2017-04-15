3f7961b61afb862544a27f3778b404dae474cef9
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-14 17:02:28 -0700

    (feat) cleaned up studentUpload path

--- src/server/cloudinary/cloudHelpers.js
+++ src/server/cloudinary/cloudHelpers.js
@@ -4,10 +4,14 @@ import Promise from 'bluebird';
 Promise.promisifyAll(cloudinary);
 
 exports.upload = async ({ studentPhoto, studentUserName, img }) => {
-  let options = { format: 'png', 'public_id': 'temporary' }
-  if (!img) {
-    options.public_id = studentUserName;
-    img = studentPhoto;
+  try {
+    let options = { format: 'png', 'public_id': 'temporary' }
+    if (!img) {
+      options.public_id = studentUserName;
+      img = studentPhoto;
+    }
+    return await cloudinary.v2.uploader.upload(img, options);
+  } catch (err) {
+    console.error(err.message);
   }
-  return await cloudinary.v2.uploader.upload(img, options);
 };
--- src/server/db/studentHelpers.js
+++ src/server/db/studentHelpers.js
@@ -6,35 +6,36 @@ import { storeInGallery } from '../kairosFR/kairosHelpers';
 
 const Student = new StudentModel();
 
-exports.addToClass =  async (req, res, next) => {
-  try {
-    const { studentUserName, selectedClass, imageLink } = req.body
+// exports.addToClass =  async (req, res, next) => {
+//   try {
+//     const { studentUserName, selectedClass, imageLink } = req.body
 
-    const addUserClass = `INSERT INTO class_user (class_id, user_id) 
-    SELECT classes.classes_id, users.users_id FROM classes, users 
-    WHERE users.user_name='${studentUserName}' 
-    AND classes.class_name='${selectedClass}'`;
+//     const addUserClass = `INSERT INTO class_user (class_id, user_id) 
+//     SELECT classes.classes_id, users.users_id FROM classes, users 
+//     WHERE users.user_name='${studentUserName}' 
+//     AND classes.class_name='${selectedClass}'`;
 
-    const updateUser = `UPDATE users SET photo='${imageLink}' 
-    WHERE user_name='${studentUserName}';`
+//     const updateUser = `UPDATE users SET photo='${imageLink}' 
+//     WHERE user_name='${studentUserName}';`
 
-    await db.queryAsync(updateUser);
-    await db.queryAsync(addUserClass);
-    next();
-  } catch (err) {
-    res.status(500).send(err);
-  }
-};
+//     await db.queryAsync(updateUser);
+//     await db.queryAsync(addUserClass);
+//     next();
+//   } catch (err) {
+//     res.status(500).send(err);
+//   }
+// };
 
-exports.addToClass = (req, res) => {
+exports.addToClass = async (req, res) => {
   try {
     let { studentPhoto, studentUserName, selectedClass } = req.body;
     const { url } = await upload(req.body);
     await Student.updateUser(url, studentUserName);
     await Student.addToClass(studentUserName, selectedClass);
-    let result = await storeInGallery(studentUserName, selectedClass, url);
-    res.sendStatus(result);
+    await storeInGallery(studentUserName, selectedClass, url);
+    res.sendStatus(201);
   } catch (err) {
+    console.log(err.message);
     res.status(500).send(err.message);
   }
 }
