b009f3989957dbf11a567da17b7ec2da125c65a2
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-15 14:19:58 -0700

    Update classHelpers.js

--- src/server/db/ModelHelpers/classHelpers.js
+++ src/server/db/ModelHelpers/classHelpers.js
@@ -15,8 +15,8 @@ exports.getClass = async (req, res) => {
 exports.addClass = async (req, res) => {
   try {
     const { className } = req.body;
-    const exists = await Class.checkIfClassExists(className);
-    if (exists[0].length === 0) {
+    const [ exists ] = await Class.checkIfClassExists(className);
+    if (exists.length === 0) {
       await Class.addClass(className);
       res.sendStatus(201);
     } else {
