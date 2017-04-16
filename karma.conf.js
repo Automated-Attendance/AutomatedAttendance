787ff1097e4e4373a83d1b1678045be17adb9d18
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-16 11:31:10 -0700

    (feat) moved all test related files into test-grabber.js

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const APP_ENTRY = path.resolve(__dirname, 'src/public/AppEntry.js');
 
 module.exports = function(config) {
   config.set({
@@ -12,24 +11,14 @@ module.exports = function(config) {
     ],
 
     preprocessors: {
-      'src/public/AppEntry.js': ['webpack', 'sourcemap'],
-      'test/test-grabber.js': ['webpack', 'coverage', 'sourcemap']
+      // 'src/public/AppEntry.js': ['webpack', 'sourcemap'],
+      'test/test-grabber.js': ['webpack', 'sourcemap']
     },
 
-    reporters: ['nyan', 'coverage'],
+    reporters: ['nyan'],
 
     nyanReporter: {
       suppressErrorHighlighting: true,
-    },
-
-    coverageReporter: {
-      reporters: [
-        {type: 'text'},
-        {type: 'text-summary'},
-        {type: 'html', dir: 'coverage/', subdir: 'report/'},
-        {type: 'lcov', dir: 'coverage/'},
-        {type: 'cobertura', dir: 'coverage/', file: 'coverage.xml'},
-      ],
     },
 
     webpack: {
@@ -75,86 +64,3 @@ module.exports = function(config) {
     singleRun: false,
   });
 };
-
-
-
-
-// const path = require('path');
-// const APP_ENTRY = path.resolve(__dirname, 'src/public/AppEntry.js');
-
-// module.exports = function(config) {
-//   config.set({
-//     basePath: '',
-//     browsers: ['PhantomJS'],
-//     frameworks: ['mocha'],
-//     files: [
-//       'node_modules/babel-polyfill/dist/polyfill.js',
-//       'src/public/AppEntry.js',
-//       'test/test-grabber.js'
-//     ],
-
-//     preprocessors: {
-//       'src/public/AppEntry.js': ['webpack', 'sourcemap'],
-//       'test/test-grabber.js': ['webpack', 'sourcemap']
-//     },
-
-//     reporters: ['nyan', 'coverage'],
-
-//     nyanReporter: {
-//       suppressErrorHighlighting: true,
-//     },
-
-//     coverageReporter: {
-//       reporters: [
-//         {type: 'text'},
-//         {type: 'text-summary'},
-//         {type: 'html', dir: 'coverage/', subdir: 'report/'},
-//         {type: 'lcov', dir: 'coverage/'},
-//         {type: 'cobertura', dir: 'coverage/', file: 'coverage.xml'},
-//       ],
-//     },
-
-//     webpack: {
-//       devtool: 'inline-source-map',
-//       module: {
-//         loaders: [
-//           {
-//             test: /\.js$/,
-//             include: APP_ENTRY,
-//             loader: 'babel-loader',
-//             exclude: path.resolve(__dirname, 'node_modules'),
-//             query: {
-//               plugins: ['transform-decorators-legacy', 'transform-regenerator'],
-//               presets: ['react', 'es2015', 'stage-1']
-//             }
-//           },
-//           { test: /\.css$/, loader: 'style-loader!css-loader' },
-//           { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
-//           { test: /\.gif$/, loader: 'url-loader?mimetype=image/png' },
-//           { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-//           { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=[name].[ext]' }
-//         ]
-//       },
-//       externals: {
-//         'react/addons': true,
-//         'react/lib/ExecutionEnvironment': true,
-//         'react/lib/ReactContext': true
-//       }
-//     },
-
-//     webpackServer: {
-//       noInfo: true
-//     },
-
-//     browserConsoleLogOptions: {
-//       level: 'log',
-//       terminal: true
-//     },
-
-//     port: 9876,
-//     colors: true,
-//     logLevel: config.LOG_INFO,
-//     autoWatch: true,
-//     singleRun: false,
-//   });
-// };
