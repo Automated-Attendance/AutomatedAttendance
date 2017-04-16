190f5cc43e90c56a14274ff3e7554225b85f1e05
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-16 11:31:37 -0700

    (feat) added components to test-grabber

--- test/test-grabber.js
+++ test/test-grabber.js
@@ -1,2 +1,5 @@
 const testsContext = require.context('.', true, /-test$/);
 testsContext.keys().forEach(testsContext);
+
+const componentsContext = require.context('../src/public/components/', true, /\.js$/);
+componentsContext.keys().forEach(componentsContext);
