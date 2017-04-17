b91ba25600064345d7d4f80cc1850bd4cc69a1e5
Han <hanshengzhao1993@gmail.com>
2017-04-17 09:56:06 -0700

    commented out setTimeOut

--- src/server/db/ModelHelpers/attendanceHelpers.js
+++ src/server/db/ModelHelpers/attendanceHelpers.js
@@ -6,6 +6,11 @@ exports.storeRecords = async (req, res) => {
   try {
     const { classes, time } = req.body;
     await Attendance.storeRecords(classes, time);
+
+    // setInterval( () => {
+    //   console.log('check every 3 sec')
+    // }, 3000);
+
     res.sendStatus(201);
   } catch (err) {
     res.status(500).send(err.message);
