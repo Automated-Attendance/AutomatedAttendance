aa3ed26429e5f8b82661048913df26d983b2ad5d
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-19 16:50:54 -0700

    (feat) setup server testing environment

--- test/ServerSideTests/example-servertest.js
+++ test/ServerSideTests/example-servertest.js
@@ -1,27 +1,63 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
-import server from '../../src/server/app';
-import db from '../../src/server/db/index';
+import app from '../../src/server/app';
+import Schema from '../../src/server/db/config';
+import mysql from 'mysql';
+import Promise from 'bluebird';
 
 // for stubbing requests
 chai.use(chaiHttp);
 
-describe('a sample server test', () => {
+describe('', () => {
 
-  after(() => {
-    setTimeout(() => {
-      db.connection.end();
-    }, 1000);
+  let db;
+  let server;
+  let port = 3456;
+  const clearDB = async (connection, tablenames, done) => {
+    try {
+      let count = 0;
+      tablenames.forEach(async (tablename) => {
+        connection.queryAsync('DROP TABLE IF EXISTS ' + tablename);
+        if (++count === tablenames.length) return Schema(db).then(done);
+      });
+    } catch (err) {
+      return done(err.message);
+    }
+  };
+
+
+
+  beforeEach((done) => {
+    db = mysql.createConnection({
+      host: process.env.CLOUD_TEST_DB_HOST,
+      user: process.env.CLOUD_TEST_DB_ADMIN,
+      password: process.env.CLOUD_TEST_DB_PASSWORD,
+    });
+
+    const tablenames = ['attendance_record', 'users', 'classes', 'class_user'];
+
+    db.connect(function(err) {
+      if (err) { return done(err); }
+      clearDB(db, tablenames, () => {
+        server = app.listen(port, done);
+      });
+    });
+
+    afterEach(() => server.close());
   });
 
-  it('should work', (done) => {
-    chai.request(server)
-      .get('/testingroute')
-      .end((err, res) => {
-        expect(res.status).to.equal(200);
-        expect(res.text).to.equal('idk man');
-        done();
-      });
+  describe('dummy test', () => {
+
+    it('should work', (done) => {
+      chai.request(server)
+        .get('/testingroute')
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.text).to.equal('idk man');
+          done();
+        });
+    });
+
   });
 
 });
