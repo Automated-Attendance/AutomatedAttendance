82748e5dc5fb6b44bf489c140097806d386c7ffa
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-19 12:19:23 -0700

    (feat) added mocha-webpack config

---
+++ mocha-webpack.config.js
@@ -0,0 +1,14 @@
+const nodeExternals = require('webpack-node-externals');
+ 
+module.exports = {
+  target: 'node',
+  externals: [nodeExternals()],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }
+    ]
+  }
+};
