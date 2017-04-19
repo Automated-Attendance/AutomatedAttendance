ba4ef1407e5d2b37299130f61e21dfbe0f76c927
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-19 12:46:50 -0700

    (feat) setup mocha-webpack config for server testing

--- mocha-webpack.config.js
+++ mocha-webpack.config.js
@@ -2,12 +2,17 @@ const nodeExternals = require('webpack-node-externals');
  
 module.exports = {
   target: 'node',
+  devtool: 'inline-source-map',
   externals: [nodeExternals()],
   module: {
     loaders: [
       {
-        test: /\.js$/,
-        loader: 'babel-loader'
+        test: /\.js?/,
+        loader: 'babel-loader',
+        query: {
+          plugins: ['transform-decorators-legacy', 'transform-regenerator', 'transform-runtime'],
+          presets: ['react', 'es2015', 'stage-1']
+        }
       }
     ]
   }
