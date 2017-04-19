6116655b19def8ac51a29be501a4b829ebca3c8b
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-19 13:25:24 -0700

    (feat) close db connection on test end

--- test/ServerSideTests/example-servertest.js
+++ test/ServerSideTests/example-servertest.js
@@ -1,6 +1,6 @@
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
-import server from '../../src/server/app.js';
+import server from '../../src/server/app';
 import db from '../../src/server/db/index';
 
 // for stubbing requests
@@ -8,11 +8,18 @@ chai.use(chaiHttp);
 
 describe('a sample server test', () => {
 
+  after(() => {
+    setTimeout(() => {
+      db.connection.end();
+    }, 1000);
+  });
+
   it('should work', (done) => {
     chai.request(server)
       .get('/testingroute')
       .end((err, res) => {
         expect(res.status).to.equal(200);
+        expect(res.text).to.equal('idk man');
         done();
       });
   });
