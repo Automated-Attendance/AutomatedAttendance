077be8775429b8f245d4cb11fee8b4ec02e090b5
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-20 16:18:02 -0700

    (feat) fix error handling in cloudHelpers function

--- src/server/cloudinary/cloudHelpers.js
+++ src/server/cloudinary/cloudHelpers.js
@@ -6,12 +6,14 @@ Promise.promisifyAll(cloudinary);
 exports.upload = async ({ studentPhoto, studentUserName, img }) => {
   try {
     let options = { format: 'png', 'public_id': 'temporary' }
+    /* istanbul ignore else  */
     if (!img) {
       options.public_id = studentUserName;
       img = studentPhoto;
     }
     return await cloudinary.v2.uploader.upload(img, options);
   } catch (err) {
-    console.error(err.message);
+    /* istanbul ignore next  */
+    res.status(500).send(err);
   }
 };
