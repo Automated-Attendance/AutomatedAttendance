fe0000f7b34bc8813772dc3aef241f5ba04a88ee
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-20 16:11:32 -0700

    (feat) ignore 500s in search Helpers

--- src/server/db/ModelHelpers/searchHelpers.js
+++ src/server/db/ModelHelpers/searchHelpers.js
@@ -11,6 +11,7 @@ exports.getAllUsernames = async (req, res) => {
     const [result] = await Search.getFirstLastGithubNames();
     res.json(result);
   } catch (err) {
+    /* istanbul ignore next */
     res.status(500).send(err.message);
   }
 };
