266f1ce97a1e9bfb0a38d8c23549f067b84e9970
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-24 13:48:17 -0700

    Change DB cloud variables from Heroku to AWS

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -18,12 +18,12 @@ if (process.env.LOCAL_ENV && process.env.CLOUD_TEST_DB) {
     password: process.env.MYSQL_PASSWORD_LOCAL,
   });
 } else {
-  database = process.env.MYSQL_DB_NAME;
+  database = process.env.RDS_DB_NAME;
   connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_ADMIN,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DB_NAME
+    host: process.env.RDS_HOSTNAME,
+    user: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_PORT
   });
 }
 
