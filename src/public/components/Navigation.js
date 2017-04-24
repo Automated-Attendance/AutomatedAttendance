7ad3f6fc15cd9d2f4c91eaa8da3e8ea77896ac82
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-24 16:00:09 -0700

    (fix) fixed spacing in Navigation component

--- src/public/components/Navigation.js
+++ src/public/components/Navigation.js
@@ -3,38 +3,34 @@ import { Link } from 'react-router-dom';
 
 const Navigation = ({ userPrivs }) => {
   return (
+    <nav className="navbar navbar-fixed-top navbar-default">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a href="/"><img src="/images/AutoAttenLogo.png" alt="Automated Attendance Logo"/></a>
+        </div>
 
-
-<nav className="navbar navbar-fixed-top navbar-default">
-  <div className="container-fluid">
-    <div className="navbar-header">
-      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-      </button>
-      <a href="/"><img src="/images/AutoAttenLogo.png" alt="Automated Attendance Logo"/></a>
-    </div>
-
-    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul className="nav navbar-nav">
-        { userPrivs.isLoggedIn && !userPrivs.isAdmin && <li><Link to="/Student"><span className="glyphicon glyphicon-th-list" aria-hidden="true"/> Attendance</Link></li> }
-        { userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/Admin"><span className="glyphicon glyphicon-th-list" aria-hidden="true"/> Attendance</Link></li> }
-        { userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/CameraPage"><span className="glyphicon glyphicon-camera" aria-hidden="true"/> Camera</Link></li> }
-        { userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/Enrollment"><span className="glyphicon glyphicon-list-alt" aria-hidden="true"/> Enrollment</Link></li> }
-      </ul>
-      <ul className="nav navbar-nav navbar-right">
-        <li><Link to="/About"><span className=" glyphicon glyphicon-globe" aria-hidden="true"/> About</Link></li>
-        <li><Link to="/Contact"><span className="glyphicon glyphicon-earphone" aria-hidden="true"/> Contact</Link></li>
-        { !userPrivs.isLoggedIn && <li><a href="/login"><span className="glyphicon glyphicon-user" aria-hidden="true"/> Login</a></li> }
-        { userPrivs.isLoggedIn && <li><a href="/logout"><span className="glyphicon glyphicon-user" aria-hidden="true"/> Logout</a></li> }
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav">
+            { userPrivs.isLoggedIn && !userPrivs.isAdmin && <li><Link to="/Student"><span className="glyphicon glyphicon-th-list" aria-hidden="true"/> Attendance</Link></li> }
+            { userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/Admin"><span className="glyphicon glyphicon-th-list" aria-hidden="true"/> Attendance</Link></li> }
+            { userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/CameraPage"><span className="glyphicon glyphicon-camera" aria-hidden="true"/> Camera</Link></li> }
+            { userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/Enrollment"><span className="glyphicon glyphicon-list-alt" aria-hidden="true"/> Enrollment</Link></li> }
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            <li><Link to="/About"><span className=" glyphicon glyphicon-globe" aria-hidden="true"/> About</Link></li>
+            <li><Link to="/Contact"><span className="glyphicon glyphicon-earphone" aria-hidden="true"/> Contact</Link></li>
+            { !userPrivs.isLoggedIn && <li><a href="/login"><span className="glyphicon glyphicon-user" aria-hidden="true"/> Login</a></li> }
+            { userPrivs.isLoggedIn && <li><a href="/logout"><span className="glyphicon glyphicon-user" aria-hidden="true"/> Logout</a></li> }
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
