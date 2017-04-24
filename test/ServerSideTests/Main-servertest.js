adf149a75695af1209087da99cf1ca1399214486
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-24 16:01:36 -0700

    (fix) turned off test db insertion

--- test/ServerSideTests/Main-servertest.js
+++ test/ServerSideTests/Main-servertest.js
@@ -34,24 +34,24 @@ describe('', function() {
     await db.queryAsync('CREATE DATABASE IF NOT EXISTS ' + dbName);
     await db.queryAsync('USE ' + dbName);
     await makeTables(db);
-    await db.queryAsync(`INSERT INTO users (user_name, first_name, last_name, email) VALUES ('hanzh', 'Han', 'Zhao', 'hanshengzhao1993@gmail.com');`);
-    await db.queryAsync(`INSERT INTO users (user_name, first_name, last_name, email) VALUES ('andrewaaalonis', 'Andrew', 'Alonis', 'myemailisthirtyninedigitslong@gmail.com');`);
-    await db.queryAsync(`INSERT INTO users (user_name, first_name, last_name, email) VALUES ('Duy12312313', 'Duy', 'Nguyen', 'duyng92@gmail.com');`);
-    await db.queryAsync(`INSERT INTO users (user_name, first_name, last_name, email) VALUES ('Jukejc', 'Jason', 'Chambers', 'jas.o.chambers@gmail.com');`);
-    await db.queryAsync(`INSERT INTO classes (class_name) VALUES ('HRSF72');`);
-    await db.queryAsync(`INSERT INTO classes (class_name) VALUES ('HRSF76');`);
-    await db.queryAsync(`INSERT INTO class_user (class_id, user_id)
-        VALUES ((SELECT classes_id FROM classes WHERE class_name='HRSF72'),
-        (SELECT users_id FROM users WHERE email='hanshengzhao1993@gmail.com'));`);
-    await db.queryAsync(`INSERT INTO class_user (class_id, user_id)
-        VALUES ((SELECT classes_id FROM classes WHERE class_name='HRSF72'),
-        (SELECT users_id FROM users WHERE email='myemailisthirtyninedigitslong@gmail.com'));`);
-    await db.queryAsync(`INSERT INTO class_user (class_id, user_id)
-        VALUES ((SELECT classes_id FROM classes WHERE class_name='HRSF72'),
-        (SELECT users_id FROM users WHERE email='jas.o.chambers@gmail.com'));`);
-    await db.queryAsync(`INSERT INTO class_user (class_id, user_id)
-        VALUES ((SELECT classes_id FROM classes WHERE class_name='HRSF76'),
-        (SELECT users_id FROM users WHERE user_name='Duy12312313'));`);
+    // await db.queryAsync(`INSERT INTO users (user_name, first_name, last_name, email) VALUES ('hanzh', 'Han', 'Zhao', 'hanshengzhao1993@gmail.com');`);
+    // await db.queryAsync(`INSERT INTO users (user_name, first_name, last_name, email) VALUES ('andrewaaalonis', 'Andrew', 'Alonis', 'myemailisthirtyninedigitslong@gmail.com');`);
+    // await db.queryAsync(`INSERT INTO users (user_name, first_name, last_name, email) VALUES ('Duy12312313', 'Duy', 'Nguyen', 'duyng92@gmail.com');`);
+    // await db.queryAsync(`INSERT INTO users (user_name, first_name, last_name, email) VALUES ('Jukejc', 'Jason', 'Chambers', 'jas.o.chambers@gmail.com');`);
+    // await db.queryAsync(`INSERT INTO classes (class_name) VALUES ('HRSF72');`);
+    // await db.queryAsync(`INSERT INTO classes (class_name) VALUES ('HRSF76');`);
+    // await db.queryAsync(`INSERT INTO class_user (class_id, user_id)
+    //     VALUES ((SELECT classes_id FROM classes WHERE class_name='HRSF72'),
+    //     (SELECT users_id FROM users WHERE email='hanshengzhao1993@gmail.com'));`);
+    // await db.queryAsync(`INSERT INTO class_user (class_id, user_id)
+    //     VALUES ((SELECT classes_id FROM classes WHERE class_name='HRSF72'),
+    //     (SELECT users_id FROM users WHERE email='myemailisthirtyninedigitslong@gmail.com'));`);
+    // await db.queryAsync(`INSERT INTO class_user (class_id, user_id)
+    //     VALUES ((SELECT classes_id FROM classes WHERE class_name='HRSF72'),
+    //     (SELECT users_id FROM users WHERE email='jas.o.chambers@gmail.com'));`);
+    // await db.queryAsync(`INSERT INTO class_user (class_id, user_id)
+    //     VALUES ((SELECT classes_id FROM classes WHERE class_name='HRSF76'),
+    //     (SELECT users_id FROM users WHERE user_name='Duy12312313'));`);
     server = app.listen(port);
 
     afterEach(() => {
