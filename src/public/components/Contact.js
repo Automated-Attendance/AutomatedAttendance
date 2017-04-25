50a7acf1ce3010cf769a5caac7293815c6f3c80e
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-25 12:08:21 -0700

    [Syntax] Contact

--- src/public/components/Contact.js
+++ src/public/components/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Contact = () => {
   return (
     <div>
-      <h3 className="header">Running Late? Or ran into a bug?</h3>
+      <h3 className="header">Running late? Or ran into a bug?</h3>
       <hr/>
       <h4>For attendance or account issues, contact:</h4>
       <a href="mailto:communication@hackreactor.com?Subject=Automated%20Attendance%20Support">communication@hackreactor.com</a>
