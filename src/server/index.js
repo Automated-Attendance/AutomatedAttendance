047de34b9da9098e979c6624feb46720fc423ce1
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-25 09:40:03 -0700

    Syntax

--- src/server/index.js
+++ src/server/index.js
@@ -2,5 +2,5 @@ import app from './app';
 import db from './db/index';
 
 app.listen(process.env.PORT || 3000, function() {
-  console.log('Listening on port 3000.');
+  console.log('Listening on port 3000');
 });
