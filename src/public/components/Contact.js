d538faa08b00d04363cfebcbfc5d1fd147520dca
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-25 10:35:23 -0700

    Update Contact.js

--- src/public/components/Contact.js
+++ src/public/components/Contact.js
@@ -5,10 +5,10 @@ const Contact = () => {
     <div>
       <h3 className="header">Running Late? Or ran into a bug?</h3>
       <hr/>
-      <h4>For attendance or account issues, please contact:</h4>
+      <h4>For attendance or account issues, contact:</h4>
       <a href="mailto:communication@hackreactor.com?Subject=Automated%20Attendance%20Support">communication@hackreactor.com</a>
       <br/><br/>
-      <h4>For technical issues, please contact:</h4>
+      <h4>For technical issues, contact:</h4>
       <a href="mailto:AAAllstars@gmail.com?Subject=Automated%20Attendance%20Support">AAAllstars@gmail.com</a>
     </div>
   );
