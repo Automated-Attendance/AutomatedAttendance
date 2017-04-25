d9f326f3b0e64a9d392ecda6671fb01e6e3856ae
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-25 12:26:05 -0700

    (feat) added styles for top part of landing page

--- src/public/components/Landing.js
+++ src/public/components/Landing.js
@@ -4,8 +4,12 @@ const Landing = () => {
   return (
     <div>
 
-      <div className="top-level-landing">
-      ya
+      <div id="top-level-landing">
+        <div className="container text-center">
+          <img src="/images/LandingLogo.png" alt="Landing Logo"/>
+          <h1 className="landing-title"> Fully Automated Attendance Tracking System </h1>
+          <h4 className="landing-slogan"> Set it and forget it. That's all it takes. </h4>
+          </div>
       </div>
 
     </div>
