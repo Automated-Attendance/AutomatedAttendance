bb80020e22eb112f7ad59eaf1bea9417a88ae500
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-25 11:44:26 -0700

    [Syntax] LoginRedirect

--- src/public/components/LoginRedirect.js
+++ src/public/components/LoginRedirect.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
-const LoginRedirect = ({ userPrivs }) => {
+const LoginRedirect = ({userPrivs}) => {
   if (userPrivs.isAdmin || userPrivs.isLoggedIn) {
     return (
       <div className="container">
         <div className="row">
-            <div className="col-md-12">
-                <div className="error-template">
-                    <h1>
-                        Oops!</h1>
-                    <h2>
-                        Access Denied</h2>
-                    <div className="error-details">
-                        You don't have correct privileges to acces this page.
-                    </div>
-                    <div className="error-actions">
-                        <a href="/" className="login-button btn btn-primary btn-lg"><span className="glyphicon glyphicon-home"></span> Take Me Home </a>
-                        <a href="/Contact" className="btn btn-default btn-lg"><span className="glyphicon glyphicon-envelope"></span> Contact Support </a>
-                    </div>
-                </div>
+          <div className="col-md-12">
+            <div className="error-template">
+              <h1>Oops!</h1>
+              <h2>Access Denied</h2>
+              <div className="error-details">
+                You don't have privileges to access this page.
+              </div>
+              <div className="error-actions">
+                <a href="/" className="login-button btn btn-primary btn-lg"><span className="glyphicon glyphicon-home"></span>Take Me Home</a>
+                <a href="/Contact" className="btn btn-default btn-lg"><span className="glyphicon glyphicon-envelope"></span>Contact Support</a>
+              </div>
             </div>
+          </div>
         </div>
       </div>
     );
@@ -28,21 +25,19 @@ const LoginRedirect = ({ userPrivs }) => {
     return (
       <div className="container">
         <div className="row">
-            <div className="col-md-12">
-                <div className="error-template">
-                    <h1>
-                        Oops!</h1>
-                    <h2>
-                        Access Denied</h2>
-                    <div className="error-details">
-                        Are you logged in? 
-                    </div>
-                    <div className="error-actions">
-                        <a href="/login" className="login-button btn btn-primary btn-lg"><span className="glyphicon glyphicon-user"></span> Log Me In </a>
-                        <a href="/Contact" className="btn btn-default btn-lg"><span className="glyphicon glyphicon-envelope"></span> Contact Support </a>
-                    </div>
-                </div>
+          <div className="col-md-12">
+            <div className="error-template">
+              <h1>Oops!</h1>
+              <h2>Access Denied</h2>
+              <div className="error-details">
+                Are you logged in? 
+              </div>
+              <div className="error-actions">
+                <a href="/login" className="login-button btn btn-primary btn-lg"><span className="glyphicon glyphicon-user"></span> Log Me In </a>
+                <a href="/Contact" className="btn btn-default btn-lg"><span className="glyphicon glyphicon-envelope"></span> Contact Support </a>
+              </div>
             </div>
+          </div>
         </div>
       </div>
     );
