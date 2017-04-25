e93315d82f1d3405ef8b8ae9bc378c0741b28fb5
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-25 12:03:18 -0700

    (feat) added landing page

---
+++ src/public/components/Landing.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+
+      <div className="top-level-landing">
+      ya
+      </div>
+
+    </div>
+  );
+};
+
+export default Landing;
