0f842fd70cbab5373888e078b1b2a80f0c44837e
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-24 22:44:40 -0700

    (feat) cleaned up db index.js

--- src/server/db/index.js
+++ src/server/db/index.js
@@ -6,7 +6,7 @@ let connection;
 
 /* istanbul ignore next */
 if (process.env.HEROKU_ENV === 'production') {
-  console.log('connecting to heroku');
+  //heroku PRODUCTION
   database = process.env.MYSQL_DB_NAME;
   connection = mysql.createConnection({
     host: process.env.MYSQL_HOST,
@@ -14,50 +14,28 @@ if (process.env.HEROKU_ENV === 'production') {
     password: process.env.MYSQL_PASSWORD,
     database: database
   });
-} else if (process.env.TRAVIS && process.env.NODE_ENV === 'test') {
-  // travis CI localhost
+} else {
+  // travis CI && local testing
   database = 'automatedattendance';
   connection = mysql.createConnection({
     user: 'root',
     password: ''
   });
-} else if (process.env.LOCAL_ENV && process.env.CLOUD_TEST_DB) {
-  // google cloud db
-  database = process.env.CLOUD_TEST_DB_NAME;
-  connection = mysql.createConnection({
-    host: process.env.CLOUD_TEST_DB_HOST,
-    user: process.env.CLOUD_TEST_DB_ADMIN,
-    password: process.env.CLOUD_TEST_DB_PASSWORD,
-  });
-} else if (process.env.LOCAL_ENV) {
-  // localhost
-  database = process.env.MYSQL_DB_NAME_LOCAL;
-  connection = mysql.createConnection({
-    user: process.env.MYSQL_ADMIN_LOCAL,
-    password: process.env.MYSQL_PASSWORD_LOCAL,
-  });
 }
 
-const db = Promise.promisifyAll(connection, { multiArgs: true });
+const initConnection = async () => {
+  try {
+    const db = Promise.promisifyAll(connection, { multiArgs: true });
+    await db.connectAsync();
+    // await db.queryAsync('DROP DATABASE IF EXISTS ' + database);
+    console.log('Connected to ' + database + 'database as ID ' + db.threadId);
+    await db.queryAsync('CREATE DATABASE IF NOT EXISTS ' + database);
+    await db.queryAsync('USE ' + database);
+    await createTables(db);
+    module.exports = db;
+  } catch (err) {
+    console.log(err.message, 'there was an error connecting to the database');
+  }
+};
 
-db.connectAsync().then(function() {
-  // keep the connection alive so server doesnt crash 
-  // setInterval(() => db.queryAsync('SELECT 1'), 5000);
-  // return db.queryAsync('DROP DATABASE IF EXISTS ' + database);
-  console.log('connected to db', database);
-})
-.then(function() {
-  console.log('Connected to ' + database + 'database as ID ' + db.threadId);
-  return db.queryAsync('CREATE DATABASE IF NOT EXISTS ' + database);
-})
-.then(function() {
-  return db.queryAsync('USE ' + database);
-})
-.then(function() {
-  return createTables(db);
-})
-.catch(function(err) {
-  console.log(err.message, 'there was an error connecting to the database');
-});
-
-module.exports = db;
+initConnection();
