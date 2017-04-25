395d6f568f9eecb40013818e506712aba4a5b76b
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-25 11:47:24 -0700

    [Syntax] Navigation

--- src/public/components/Navigation.js
+++ src/public/components/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
-const Navigation = ({ userPrivs }) => {
+const Navigation = ({userPrivs}) => {
   return (
     <nav className="navbar navbar-fixed-top navbar-default">
       <div className="container-fluid">
@@ -17,16 +17,16 @@ const Navigation = ({ userPrivs }) => {
 
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul className="nav navbar-nav">
-            { userPrivs.isLoggedIn && !userPrivs.isAdmin && <li><Link to="/Student"><span className="glyphicon glyphicon-th-list" aria-hidden="true"/> Attendance</Link></li> }
-            { userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/Admin"><span className="glyphicon glyphicon-th-list" aria-hidden="true"/> Attendance</Link></li> }
-            { userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/CameraPage"><span className="glyphicon glyphicon-camera" aria-hidden="true"/> Camera</Link></li> }
-            { userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/Enrollment"><span className="glyphicon glyphicon-list-alt" aria-hidden="true"/> Enrollment</Link></li> }
+            {userPrivs.isLoggedIn && !userPrivs.isAdmin && <li><Link to="/Student"><span className="glyphicon glyphicon-th-list" aria-hidden/>Attendance</Link></li>}
+            {userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/Admin"><span className="glyphicon glyphicon-th-list" aria-hidden/>Attendance</Link></li>}
+            {userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/CameraPage"><span className="glyphicon glyphicon-camera" aria-hidden/>Camera</Link></li>}
+            {userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/Enrollment"><span className="glyphicon glyphicon-list-alt" aria-hidden/>Enrollment</Link></li>}
           </ul>
           <ul className="nav navbar-nav navbar-right">
-            <li><Link to="/About"><span className=" glyphicon glyphicon-globe" aria-hidden="true"/> About</Link></li>
-            <li><Link to="/Contact"><span className="glyphicon glyphicon-earphone" aria-hidden="true"/> Contact</Link></li>
-            { !userPrivs.isLoggedIn && <li><a href="/login"><span className="glyphicon glyphicon-user" aria-hidden="true"/> Login</a></li> }
-            { userPrivs.isLoggedIn && <li><a href="/logout"><span className="glyphicon glyphicon-user" aria-hidden="true"/> Logout</a></li> }
+            <li><Link to="/About"><span className=" glyphicon glyphicon-globe" aria-hidden/>About</Link></li>
+            <li><Link to="/Contact"><span className="glyphicon glyphicon-earphone" aria-hidden/>Contact</Link></li>
+            {!userPrivs.isLoggedIn && <li><a href="/login"><span className="glyphicon glyphicon-user" aria-hidden/>Login</a></li>}
+            {userPrivs.isLoggedIn && <li><a href="/logout"><span className="glyphicon glyphicon-user" aria-hidden/>Logout</a></li>}
           </ul>
         </div>
       </div>
