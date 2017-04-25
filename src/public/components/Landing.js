befe9c5e5370da20247c4628d934baa3d0684e84
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-25 12:29:22 -0700

    (refactor) fixed slogan

--- src/public/components/Landing.js
+++ src/public/components/Landing.js
@@ -8,8 +8,13 @@ const Landing = () => {
         <div className="container text-center">
           <img src="/images/LandingLogo.png" alt="Landing Logo"/>
           <h1 className="landing-title"> Fully Automated Attendance Tracking System </h1>
-          <h4 className="landing-slogan"> Set it and forget it. That's all it takes. </h4>
-          </div>
+          <h4 className="landing-slogan"> Sign up.  Populate Records.  Start Camera.</h4>
+          <h4> That's all it takes. </h4>
+        </div>
+      </div>
+
+      <div className="how-it-works">
+      How it works here.
       </div>
 
     </div>
