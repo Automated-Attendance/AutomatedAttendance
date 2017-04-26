a31c489be47c8746de63c705e8fe18c8b357232c
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-26 12:17:00 -0700

    Extract StartAttendance

--- src/public/components/CameraPage.js
+++ src/public/components/CameraPage.js
@@ -1,44 +1,40 @@
 import React from 'react';
-import Webcam from 'react-webcam';
-import keydown, { Keys } from 'react-keydown';
-import { queryGallery } from '../requests/gallery';
-import Select from 'react-select';
 import Spinner from './Spinner';
-import DateTime from 'react-widgets/lib/DateTimePicker';
-import { getClasses } from '../requests/classes';
 import Moment from 'moment';
-import { storeAttendanceRecord, emailLateStudents } from '../requests/students';
-
+import Webcam from 'react-webcam';
+import {getClasses} from '../requests/classes';
+import {queryGallery} from '../requests/gallery';
+import {storeAttendanceRecord, emailLateStudents} from '../requests/students';
+import StartAttendance from './StartAttendance';
 
 export default class CameraPage extends React.Component {
-
   constructor(props) {
     super(props);
-
     this.state = {
       value: '',
       options: [],
       spinner: false,
+      attendancePopulated: false,
       checkedinUser: null,
       selectedTimeCutoff: null,
-      attendancePopulated: false,
       intervalId: null
     };
 
-    'sendLateEmails',
-    ['takeScreenshot',
-    'getSelectOptions',
-    'handleSelectChange',
-    'populateAttendanceRecord',
+    ['getSelectOptions',
     'updateSelectedTimeCutoff',
-    'toggleOff', 
-    'clearDOMValue'].forEach(method => {
+    'populateAttendanceRecord',
+    'startCamera',
+    'takeScreenshot',
+    'handleSelectChange',
+    'handleSubmitStartAttendance',
+    'sendLateEmails'].forEach(method => {
       this[method] = this[method].bind(this);
     });
   }
 
   componentWillMount() {
     this.setState({mounted: true});
+    this.getSelectOptions();
   }
 
   componentWillUnmount() {
@@ -46,21 +42,28 @@ export default class CameraPage extends React.Component {
     clearInterval(this.state.intervalId);
   }
 
-  async takeScreenshot() {
-    const screenshot = this.refs.webcam.getScreenshot();
-    this.setState({spinner: true});
-    const checkedIn = await queryGallery(screenshot);
-    let checkedInStudents = [];
-    if (checkedIn.length) {
-      checkedIn.forEach(student => checkedInStudents.push(`${student.first_name}${student.last_name ? ' ' + student.last_name : ''}`));
-    }
-    this.setState({spinner: false, checkedinUser: `Checked in: ${checkedInStudents.join(', ')}!`});
+  async getSelectOptions() {
+    const classList = await getClasses();
+    const classes = classList.classes.map(classname => {
+      return {label: classname, value: classname};
+    });
+    this.setState({options: classes});
   }
 
-  clearDOMValue(ref) {
-    setTimeout(() => {
-      this.refs[ref]._values.value = null
-    }, 4000);
+  updateSelectedTimeCutoff(e) {
+    let date = Moment(e).format('YYYY-MM-DD HH:mm:ss');
+    this.setState({selectedTimeCutoff: date});
+  }
+
+  async populateAttendanceRecord() {
+    if (this.state.value && this.state.selectedTimeCutoff) {
+      this.setState({spinner: true, statusUpdated: false});
+      this.setState({attendancePopulated: await storeAttendanceRecord(this.state.value, this.state.selectedTimeCutoff)});
+      this.setState({spinner: false});
+    } else {
+      alert('Select Class(es) and Cutoff Time!');
+    }
+    this.setState({attendancePopulated: true});
   }
 
   startCamera() {
@@ -75,114 +78,48 @@ export default class CameraPage extends React.Component {
     this.setState({intervalId});
   }
 
-  async sendLateEmails () {
-    await emailLateStudents(this.state.selectedTimeCutoff);
-  }
-
-  async populateAttendanceRecord() {
-    if (this.state.value && this.state.selectedTimeCutoff) {
-      this.setState({spinner: true, statusUpdated: false});
-      this.setState({attendancePopulated: await storeAttendanceRecord(this.state.value, this.state.selectedTimeCutoff)});
-      this.setState({spinner: false});
-    } else {
-      alert('Select Class(es) and Cutoff Time!');
+  async takeScreenshot() {
+    const screenshot = this.refs.webcam.getScreenshot();
+    this.setState({spinner: true});
+    const checkedIn = await queryGallery(screenshot);
+    let checkedInStudents = [];
+    if (checkedIn.length) {
+      checkedIn.forEach(student => checkedInStudents.push(`${student.first_name}${student.last_name ? ' ' + student.last_name : ''}`));
     }
-    this.toggleOff('attendancePopulated', 'value', 'selectedTimeCutoff');
-  }
-
-  updateSelectedTimeCutoff(e) {
-    let date = Moment(e).format('YYYY-MM-DD HH:mm:ss');
-    this.setState({selectedTimeCutoff: date});
-  }
-
-  async getSelectOptions() {
-    const classList = await getClasses();
-    const classes = classList.classes.map(classname => {
-      return {label: classname, value: classname};
-    });
-    this.setState({options: classes});
+    this.setState({spinner: false, checkedinUser: `Checked in: ${checkedInStudents.length > 0 ? checkedInStudents.join(', ') + '!' : ''}`});
   }
 
   handleSelectChange(value) {
     this.setState({value});
   }
 
-  toggleOff(status, ...states) {
-    setTimeout(() => {
-      if (status) {
-        this.setState({[status]: false});
-      }
-      states.forEach(state => {
-        if (typeof this.state[state] === 'boolean') {
-          this.setState({[state]: false});
-        } else if (Array.isArray(this.state[state])) {
-          this.setState({[state]: []})
-        } else {
-          this.setState({[state]: null})
-        }
-      });
-    }, 5000);
+  async handleSubmitStartAttendance() {
+    await this.populateAttendanceRecord();
+    this.startCamera();
+  }
+
+  async sendLateEmails () {
+    await emailLateStudents(this.state.selectedTimeCutoff);
   }
 
   render() {
     return (
       <div className="container">
-
-        { this.state.mounted && <div className="col-md-8 webcam-component"><Webcam ref='webcam'/></div> }
-
+        {this.state.mounted && <div className="col-md-8 webcam-component"><Webcam ref='webcam'/></div>}
         {this.state.spinner && <Spinner/>}
         {this.state.checkedinUser}
-
-        <div className="col-md-4 start-attendance-form">
-
-          <h3 className="text-center">Start Daily Attendance</h3>
-
-          <hr />
-
-          <h5 className="form-title">Class:</h5>
-          <div className="classSelect" onClick={!this.state.options.length && this.getSelectOptions}>
-            <Select 
-              placeholder="Select Class(es)..."
-              value={this.state.value}
-              options={this.state.options}
-              onChange={this.handleSelectChange}
-              simpleValue
-            />
-          </div>
-
-          <br/>
-
-          <h5 className="form-title">Cutoff Time:</h5>
-          <DateTime
-            placeholder="Select Cutoff Time..."
-            onChange={this.updateSelectedTimeCutoff}
-            calendar={false}
-            ref='DateTime'
-          />
-
-          <br/>
-          
-          <button
-            className="populateAttendanceRecord login-button btn btn-primary"
-            onClick={async () => {
-              await this.populateAttendanceRecord();
-              this.toggleOff(null, 'options', 'selectedTimeCutoff');
-              this.clearDOMValue('DateTime');
-              this.startCamera();
-            }}>
-            Start Today's Attendance
-          </button>
-
-          {!this.state.attendancePopulated ? null : 
-            <h5>
-              Populated daily attendance for {this.state.value} on {Moment(this.state.selectedTimeCutoff).format('dddd, MMM Do, YYYY')}!<br/>
-              You must remain on this page for the camera to continue monitoring attendance.<br/>
-              If you populated the wrong time, you must go to the Attendance page to delete today's records before resubmitting this form. 
-            </h5>
-          }
-        </div>
-
+        <StartAttendance
+          attendancePopulated={this.state.attendancePopulated}
+          selectedClass={this.state.value}
+          selectedTime={this.state.selectedTimeCutoff}
+          classOptions={this.state.options}
+          handleChangeSelect={this.handleSelectChange}
+          handleChangeTime={this.updateSelectedTimeCutoff}
+          startCamera={this.startCamera}
+          clearDOM={this.clearDOMValue}
+          handleSubmit={this.handleSubmitStartAttendance}
+        />
       </div>
     );
   }
-}
+};
---
+++ src/public/components/StartAttendance.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Moment from 'moment';
+import momentLocalizer from 'react-widgets/lib/localizers/moment';
+import DateTime from 'react-widgets/lib/DateTimePicker';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css'
+
+// init time localization for DateTimePicker
+momentLocalizer(Moment);
+
+export default class StartAttendance extends React.Component {
+  render() {
+    return (
+      <div className="col-md-4 start-attendance-form">
+        <h3 className="text-center">Start Daily Attendance</h3>
+        <hr />
+        <h5 className="form-title">Class:</h5>
+        <div className="classSelect">
+          <Select
+            placeholder="Select Class(es)..."
+            value={this.props.selectedClass}
+            options={this.props.classOptions}
+            onChange={this.props.handleChangeSelect}
+            simpleValue
+          />
+        </div>
+        <br/>
+        <h5 className="form-title">Cutoff Time:</h5>
+        <DateTime
+          placeholder="Select Cutoff Time..."
+          onChange={this.props.handleChangeTime}
+          calendar={false}
+          ref='DateTime'
+        />
+        <br/>
+        <button
+          className="populateAttendanceRecord login-button btn btn-primary"
+          onClick={this.props.handleSubmit}
+        >
+          Start Today's Attendance
+        </button>
+        {!this.props.attendancePopulated ? null : 
+          <h5>
+            Populated daily attendance for {this.props.selectedClass} on {Moment(this.props.selectedTime).format('dddd, MMM Do, YYYY')}!<br/><br/>
+            YOU MUST REMAIN ON THIS PAGE for the camera to continue monitoring attendance.<br/><br/>
+            If you populated the wrong time, you can go to the Attendance page to delete today's records before resubmitting this form. 
+          </h5>
+        }
+      </div>
+    );
+  }
+};
