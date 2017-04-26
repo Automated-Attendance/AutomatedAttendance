076388647234cad39827a93a656f214c3f0555cf
Andrew Alonis <andrew.alonis@gmail.com>
2017-04-26 15:36:26 -0700

    Uncomment tests to pending

--- test/FrontEndTests/ToggleStatus-test.js
+++ test/FrontEndTests/ToggleStatus-test.js
@@ -8,22 +8,22 @@ import ToggleStatus from '../../src/public/components/ToggleStatus';
 
 describe('<ToggleStatus/> handleSubmitToggleStatus()', () => {
 
-  // it ('should call handleSubmitToggleStatus on form click', () => {
-  //   const testFn = sinon.spy(ToggleStatus.prototype, 'handleSubmitToggleStatus');
-  //   const wrapper = mount(<ToggleStatus/>);
-  //   wrapper.setState({selectedStudentToggleStatus: {label: 'Duy Nguyen - nguyenaiden' , value: 'nguyenaiden'}, selectedToggleStatus: 'Student'});
-  //   expect(testFn.called).to.equal(false);
-  //   wrapper.find('.handleSubmitToggleStatus').simulate('click');
-  //   expect(testFn.called).to.equal(true);
-  //   testFn.restore();
-  // });
+  xit ('should call handleSubmitToggleStatus on form click', () => {
+    const testFn = sinon.spy(ToggleStatus.prototype, 'handleSubmitToggleStatus');
+    const wrapper = mount(<ToggleStatus/>);
+    wrapper.setState({selectedStudentToggleStatus: {label: 'Duy Nguyen - nguyenaiden' , value: 'nguyenaiden'}, selectedToggleStatus: 'Student'});
+    expect(testFn.called).to.equal(false);
+    wrapper.find('.handleSubmitToggleStatus').simulate('click');
+    expect(testFn.called).to.equal(true);
+    testFn.restore();
+  });
 
-  // it ('should not toggle status if states are empty', () => {
-  //   const testFn = sinon.spy(ToggleStatus.prototype, 'handleSubmitToggleStatus');
-  //   const wrapper = shallow(<ToggleStatus/>);
-  //   wrapper.find('.handleSubmitToggleStatus').simulate('click');
-  //   expect(wrapper.state().studentStatusToggled).to.equal(false);
-  //   testFn.restore();
-  // });
+  xit ('should not toggle status if states are empty', () => {
+    const testFn = sinon.spy(ToggleStatus.prototype, 'handleSubmitToggleStatus');
+    const wrapper = shallow(<ToggleStatus/>);
+    wrapper.find('.handleSubmitToggleStatus').simulate('click');
+    expect(wrapper.state().studentStatusToggled).to.equal(false);
+    testFn.restore();
+  });
 
 });
