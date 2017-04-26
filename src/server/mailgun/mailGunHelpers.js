b2dcabab7d435fef59ae23c64a6fe7bc43ee7756
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-26 10:41:51 -0700

    (refactor) removed unused function in mialgunhelpers

--- src/server/mailgun/mailGunHelpers.js
+++ src/server/mailgun/mailGunHelpers.js
@@ -1,26 +1,5 @@
 import MailGun from 'mailgun-js'
 const mailgunAPI = MailGun({apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN});
- 
-exports.sendMailLate = (req, res) => {
-  req.params.forEach( (person)=> {
-    var data = { 
-      from: 'AA Support Team <no-reply@mail.automatedattendance.com>',
-      to: person.email,
-      subject: 'Late Email Warning',
-      text: '1 minutes till class starts! Email communication@ if you are going to be late.'
-    };
-    
-    // mailgunAPI.messages().send(data, function (error, body) {
-    //   if (error) {
-    //     res.status(500).send(error);
-    //     return;
-    //     console.log(error);
-    //   } else {
-    //     console.log(body);
-    //   }
-    // });
-  });
-};
   
 exports.sendMailForArrival = async (matchedUsers) => {
   console.log('sending email for arrival');
