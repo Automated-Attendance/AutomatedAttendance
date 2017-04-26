ce6b485c7f5f148c39b14484136799e9b137d40f
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-25 21:15:14 -0700

    (feat) laid out footer for meet dev

--- src/public/components/LandingPage/Footer.js
+++ src/public/components/LandingPage/Footer.js
@@ -1,13 +1,36 @@
 import React from 'react';
 
-const TopLevel = () => {
+const Footer = () => {
   return (
     <div className="footer-container">
-      <div className="container">
-      ya da ya da da
+      <div className="text-center center-block">
+        <h4>- Meet the Developers -</h4>
+
+        <br />
+
+        <div className="col-md-3">
+        1
+        </div>
+
+        <div className="col-md-3">
+        2
+        </div>
+
+        <div className="col-md-3">
+        3
+        </div>
+
+        <div className="col-md-3">
+        4
+        </div>
       </div>
     </div>
   );
 };
 
-export default TopLevel;
+export default Footer;
+
+        // <a href="https://www.facebook.com/bootsnipp"><i className="fa fa-facebook-square fa-3x social"></i></a>
+        // <a href="https://twitter.com/bootsnipp"><i className="fa fa-twitter-square fa-3x social"></i></a>
+        // <a href="https://plus.google.com/+Bootsnipp-page"><i className="fa fa-google-plus-square fa-3x social"></i></a>
+        // <a href="mailto:bootsnipp@gmail.com"><i className="fa fa-envelope-square fa-3x social"></i></a>
