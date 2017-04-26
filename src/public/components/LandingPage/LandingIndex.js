a3938bc7e1e05e3cfb689c243e03ec6d2057cd1c
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-25 19:40:33 -0700

    (refactor) fixed styling for testimonials

--- src/public/components/LandingPage/LandingIndex.js
+++ src/public/components/LandingPage/LandingIndex.js
@@ -11,7 +11,7 @@ const Landing = () => {
 
     <HowItWorks />
 
-    <Testimonials className="container"/>
+    <Testimonials />
 
     </div>
   );
--- src/public/components/LandingPage/Testimonials.js
+++ src/public/components/LandingPage/Testimonials.js
@@ -3,6 +3,14 @@ import React from 'react';
 const Testimonials = () => {
   return (
     <div className="testimonials-container">
+
+      <div className="container">
+
+        <h1 className="text-center testimonials-text"> See what people are saying </h1>
+
+        <div> yee</div>
+      </div>
+
     </div>
   );
 };
