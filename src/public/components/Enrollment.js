0b3fca0a438cf8e323b5c50869c0845c6800f770
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-27 15:09:03 -0700

    (fix) fixed prop name

--- src/public/components/Enrollment.js
+++ src/public/components/Enrollment.js
@@ -313,6 +313,7 @@ export default class Enrollment extends React.Component {
             </div>
           </div>
         </div>
+        
         <div className="col-md-offset-2 col-md-8">
           <EnrollmentTable
             classOptions={this.state.classOptionsEnrollment}
