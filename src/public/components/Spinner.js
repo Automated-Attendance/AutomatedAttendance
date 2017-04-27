fb061bc5cbf02156bb2b86a5d03dc6b3d333f95a
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-27 11:33:21 -0700

    (feat) added spinner styling

--- src/public/components/Spinner.js
+++ src/public/components/Spinner.js
@@ -9,33 +9,35 @@ export default class Spinner extends React.Component {
 
   componentDidMount() {
     const options = {
-      lines: 13,
-      length: 28,
-      width: 14,
-      radius: 42,
-      scale: 1,
+      lines: 13, 
+      length: 22,
+      width: 8,
+      radius: 25,
+      scale: 0.75,
       corners: 1,
-      color: '#000',
-      opacity: 0.1,
-      rotate: 0,
+      color: '#0090D9',
+      opacity: 0.25,
+      rotate: 0, 
       direction: 1,
-      speed: 0.8,
-      trail: 60,
-      fps: 20,
+      speed: 0.8, 
+      trail: 60, 
+      fps: 20, 
       zIndex: 2e9,
       className: 'spinner',
-      top: '50%',
+      top: '50%', 
       left: '50%',
-      shadow: true,
-      hwaccel: false,
-      position: 'relative'
+      shadow: false,
+      hwaccel: false
     };
 
     this.spinner = new CircleSpinner(options);
-    this.spinner.spin(this.refs.spinnercircle);
+    this.spinner.spin(document.getElementById('spinnerdiv'));
   }
 
   render() {
-    return (<div ref="spinnercircle"/>);
+    return (
+      <div id="spinnerdiv">
+      </div>
+    );
   }
 }
