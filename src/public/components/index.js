eeb42147423886c65324eb5a3ddf3eb14593d7cc
Jason Chambers <jas.o.chambers@gmail.com>
2017-04-27 11:39:09 -0700

    (feat) added toggleSpinner function

--- src/public/components/index.js
+++ src/public/components/index.js
@@ -8,17 +8,24 @@ import Spinner from './Spinner';
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       isLoggedIn: false,
       isAdmin: false,
       userEmail: null,
-      spinner: true
+      spinner: false
     };
+
+    this.toggleSpinner = this.toggleSpinner.bind(this);
   }
 
   async componentWillMount() {
     const loginStatus = await getUserData();
     this.setState(loginStatus);
+  }
+
+  toggleSpinner() {
+    this.setState({ spinner: !this.state.spinner });
   }
 
   render () {
@@ -27,7 +34,10 @@ export default class App extends React.Component {
         <div>
           <Navigation userPrivs={this.state}/>
           {this.state.spinner && <Spinner/>}
-          <Routes userPrivs={this.state}/>
+          <Routes 
+            userPrivs={this.state}
+            toggleSpinner={this.toggleSpinner}
+          />
         </div>
       </Router>
     );
