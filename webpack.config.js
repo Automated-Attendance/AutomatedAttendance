b979bc70e977c3dc3ea8bc4edac614751f3bdca7
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-06 14:29:17 -0700

    (refactor) cleaned up webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,17 +14,8 @@ module.exports = {
       {
         test: /\.jsx?/,
         include: SRC_DIR,
-        loader: 'babel-loader',
-        query: {
-          plugins: ['transform-decorators-legacy', 'transform-regenerator'],
-          presets: ['react', 'es2015', 'stage-1']
-        }
+        loader: 'babel-loader'
       },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
-      { test: /\.gif$/, loader: 'url-loader?mimetype=image/png' },
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=[name].[ext]' },
       {
         test: /(\.scss$)/,
         loaders: [{
@@ -38,7 +29,12 @@ module.exports = {
             includePaths: ['./node_modules']
           }
         }]
-      }
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.less$/, loader: 'style-loader!css-loader!less-loader' },
+      { test: /\.gif$/, loader: 'url-loader?mimetype=image/png' },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader?name=[name].[ext]' },
     ]
   }
 };
