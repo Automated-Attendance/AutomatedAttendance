4add5de8f4ce9de6a0b6cb45f7b43ab8348149df
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-12 15:46:32 -0700

    (refactor) removed old code

--- src/public/actions/UserActions.js
+++ src/public/actions/UserActions.js
@@ -25,27 +25,3 @@ export function fetchLoginStatus() {
 
   }
 }
-
-
-
-// const getUserData = async () => {
-//   try {
-//     const { data } = await axios.get('/retrieveUserData');
-//     const loggedIn = data !== 'not logged in';
-//     const admin = data[0].type === 'admin';
-//     let status = { isLoggedIn: false, isAdmin: false, userEmail: null };
-//     if (loggedIn && admin) {
-//       status.isAdmin = true;
-//     }
-
-//     if (loggedIn) {
-//       status.isLoggedIn = true;
-//       status.userEmail = data[0].email;
-//     }
-
-//     return status;
-//   } catch (err) {
-//     // todo: better error handling in client
-//     console.error(err.message);
-//   }
-// };
