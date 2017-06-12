fd07814ce6ab3784ba5687ec5a6e621582a1dead
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-12 15:46:52 -0700

    (feat) connect navbar to userAuth reducer

--- src/public/components/Navigation.js
+++ src/public/components/Navigation.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
 
-const Navigation = ({userPrivs}) => {
+const Navigation = ({ isAdmin, isLoggedIn }) => {
   return (
     <nav className="navbar navbar-fixed-top navbar-default">
       <div className="container-fluid">
@@ -17,16 +18,16 @@ const Navigation = ({userPrivs}) => {
 
         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul className="nav navbar-nav">
-            {userPrivs.isLoggedIn && !userPrivs.isAdmin && <li><Link to="/Student"><span className="glyphicon glyphicon-th-list" aria-hidden/> Attendance</Link></li>}
-            {userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/Admin"><span className="glyphicon glyphicon-th-list" aria-hidden/> Attendance</Link></li>}
-            {userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/Camera"><span className="glyphicon glyphicon-camera" aria-hidden/> Camera</Link></li>}
-            {userPrivs.isLoggedIn && userPrivs.isAdmin && <li><Link to="/Enrollment"><span className="glyphicon glyphicon-list-alt" aria-hidden/> Enrollment</Link></li>}
+            {isLoggedIn && !isAdmin && <li><Link to="/Student"><span className="glyphicon glyphicon-th-list" aria-hidden/> Attendance</Link></li>}
+            {isLoggedIn && isAdmin && <li><Link to="/Admin"><span className="glyphicon glyphicon-th-list" aria-hidden/> Attendance</Link></li>}
+            {isLoggedIn && isAdmin && <li><Link to="/Camera"><span className="glyphicon glyphicon-camera" aria-hidden/> Camera</Link></li>}
+            {isLoggedIn && isAdmin && <li><Link to="/Enrollment"><span className="glyphicon glyphicon-list-alt" aria-hidden/> Enrollment</Link></li>}
           </ul>
           <ul className="nav navbar-nav navbar-right">
             <li><Link to="/About"><span className=" glyphicon glyphicon-globe" aria-hidden/> About</Link></li>
             <li><Link to="/Contact"><span className="glyphicon glyphicon-earphone" aria-hidden/> Contact</Link></li>
-            {!userPrivs.isLoggedIn && <li><a href="/login"><span className="glyphicon glyphicon-user" aria-hidden/> Login</a></li>}
-            {userPrivs.isLoggedIn && <li><a href="/logout"><span className="glyphicon glyphicon-user" aria-hidden/> Logout</a></li>}
+            {!isLoggedIn && <li><a href="/login"><span className="glyphicon glyphicon-user" aria-hidden/> Login</a></li>}
+            {isLoggedIn && <li><a href="/logout"><span className="glyphicon glyphicon-user" aria-hidden/> Logout</a></li>}
           </ul>
         </div>
       </div>
@@ -34,4 +35,12 @@ const Navigation = ({userPrivs}) => {
   );
 };
 
-export default Navigation;
+
+function mapStateToProps({ userStatus }) {
+  return {
+    isAdmin: userStatus.isAdmin,
+    isLoggedIn: userStatus.isLoggedIn
+  };
+}
+
+export default connect(mapStateToProps)(Navigation);
