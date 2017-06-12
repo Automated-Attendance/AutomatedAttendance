c68cb923d3b56b317047fcffe02da842547901f7
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-12 16:19:57 -0700

    (feat) moved routes to app component

--- src/public/components/index.js
+++ src/public/components/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import Routes from './Routes';
-import Navigation from './Navigation';
-import {BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Redirect, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { fetchLoginStatus } from '../actions/userActions';
+
+import Routes from './Routes';
+import Navigation from './Navigation';
+import Camera from './Camera';
+import Student from './Student';
+import Admin from './Admin';
+import About from './About';
+import Contact from './Contact';
+import Enrollment from './Enrollment';
+import LoginRedirect from './LoginRedirect';
+import Landing from './LandingPage/LandingIndex';
 
 class App extends React.Component {
 
@@ -12,15 +21,31 @@ class App extends React.Component {
   }
 
   render () {
+    const { isLoggedIn, isAdmin } = this.props;
     return (
       <Router>
         <div>
           <Navigation />
-          <Routes />
+          <Switch>
+            <Route path="/About" component={About}/>
+            <Route path="/Contact" component={Contact}/>
+            <Route path="/Student" component={() => isLoggedIn && !isAdmin ? <Student /> : <LoginRedirect />}/>
+            <Route path="/Camera" component={() => isAdmin ? <Camera/> : <LoginRedirect />}/>
+            <Route path="/Admin" component={() => isAdmin ? <Admin/> : <LoginRedirect />}/>
+            <Route path="/Enrollment" component={() => isAdmin ? <Enrollment/> : <LoginRedirect />}/>
+            <Route path="/" component={Landing}/>
+          </Switch>
         </div>
       </Router>
     );
   }
 };
 
-export default connect(null, { fetchLoginStatus })(App);
+function mapStateToProps({ userStatus }) {
+  return {
+    isAdmin: userStatus.isAdmin,
+    isLoggedIn: userStatus.isLoggedIn
+  };
+}
+
+export default connect(mapStateToProps, { fetchLoginStatus })(App);
