35ac06e84b475107b5a42784b402762fa56b3246
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-12 14:15:24 -0700

    (feat) connect redux store to app

--- src/public/AppEntry.js
+++ src/public/AppEntry.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/index.js';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import App from './components/index.js';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
