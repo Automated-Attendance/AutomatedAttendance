f898b0f4f18eedc5c259e39795f3bee07b3de454
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-12 17:07:46 -0700

    (feat) add GET_LOGIN_STATUS action type

--- src/public/actions/types.js
+++ src/public/actions/types.js
@@ -1,6 +1,6 @@
 //User Auth
-export const FETCH_LOGIN_STATUS = 'FETCH_LOGIN_STATUS';
-export const ERROR_FETCHING_USER = 'ERROR_FETCHING_USER';
+export const GET_LOGIN_STATUS = 'GET_LOGIN_STATUS';
+export const ERROR_GETTING_USER = 'ERROR_GETTING_USER';
 export const GET_ALL_USERS = 'GET_ALL_USERS';
 
 //
