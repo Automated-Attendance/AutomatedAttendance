40399491f375523b4db60571c3df9d2791557ba9
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-12 18:28:02 -0700

    (feat) connect Student to store and remove unused method

--- src/public/components/Student.js
+++ src/public/components/Student.js
@@ -1,75 +1,53 @@
 import React from 'react';
-import {getAttendanceRecords} from '../requests/classes';
-import StudentAttendanceTable from './tables/StudentAttendanceTable';
 import { connect } from 'react-redux';
+import { getAttendance } from '../actions/StudentRecordActions';
+import StudentAttendanceTable from './tables/StudentAttendanceTable';
 
 class Student extends React.Component {
   constructor(props) {
     super(props);
     
     this.state = {
-      attendance: [],
-      classes: {},
-      statuses: {},
       attendanceInterval: null
     };
-
-    this.getAttendance = this.getAttendance.bind(this);
   }
 
   async componentWillMount() {
-    await this.getAttendance();
-    let attendanceInterval = setInterval(async () => {
-      await this.getAttendance();
+    this.props.getAttendance(this.props.userEmail)
+    const attendanceInterval = setInterval(async () => {
+      this.props.getAttendance(this.props.userEmail)
     }, 5000);
-    this.setState({attendanceInterval});
+    this.setState({ attendanceInterval });
   }
 
   componentWillUnmount() {
     clearInterval(this.state.attendanceInterval);
   }
 
-  async getAttendance() {
-    const userEmail = this.props.userEmail;
-    const attendanceRecords = await getAttendanceRecords({email: this.props.userEmail, queryType: 'studentAttendance'});
-    attendanceRecords.forEach(item => {
-      /* istanbul ignore else  */
-      if (!this.state.classes[item.class_name]) {
-        let thisClass = this.state.classes;
-        thisClass[item.class_name] = item.class_name;
-        this.setState({classes: thisClass});
-      }
-      /* istanbul ignore else  */
-      if (!this.state.statuses[item.status]) {
-        let thisStatus = this.state.statuses;
-        thisStatus[item.status] = item.status;
-        this.setState({statuses: thisStatus});
-      }
-      let fullName = `${item.first_name}${item.last_name !== 'undefined' ? ' ' + item.last_name : ''}`;
-      item.full_name = fullName;
-    });
-    this.setState({attendance: attendanceRecords});
-  }
-
   render() {
+    const { attendance, classes, statuses } = this.props;
     return (
       <div className="attendance-page-form container">
         <h3 className="text-center">My Attendance History</h3>
         <hr/>
         <StudentAttendanceTable
-          attendance={this.state.attendance}
-          classes={this.state.classes}
-          statuses={this.state.statuses}
+          attendance={attendance}
+          classes={classes}
+          statuses={statuses}
         />
       </div>
     );
   }
 }
 
-function mapStateToProps({ userStatus }) {
+function mapStateToProps({ userStatus, studentRecords }) {
+  const { attendance, classes, statuses } = studentRecords;
   return {
+    attendance,
+    classes,
+    statuses,
     userEmail: userStatus.userEmail
   };
 }
 
-export default connect(mapStateToProps)(Student);
+export default connect(mapStateToProps, { getAttendance })(Student);
