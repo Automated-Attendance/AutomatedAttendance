dae661228b626c82881add77fd2e499c918a09c1
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-20 20:37:29 -0700

    (refactor) comment out outdated tests

--- test/FrontEndTests/index-test.js
+++ test/FrontEndTests/index-test.js
@@ -1,62 +1,62 @@
-import React from 'react';
-import { shallow, mount, render } from 'enzyme';
-import sinon from 'sinon';
-import { expect } from 'chai';
-import axios from 'axios';
-import * as users from '../../src/public/requests/users';
-import App from '../../src/public/components/index';
+// import React from 'react';
+// import { shallow, mount, render } from 'enzyme';
+// import sinon from 'sinon';
+// import { expect } from 'chai';
+// import axios from 'axios';
+// import * as users from '../../src/public/requests/users';
+// import App from '../../src/public/components/index';
 
-describe('<App />', function() {
+// describe('<App />', function() {
 
-  let sandbox;
-  beforeEach(() => {
-    sandbox = sinon.sandbox.create();
-    const resolved = new Promise((res) => res({ data: [{ type: 'admin' }] }));
-    sandbox.stub(axios, 'get').returns(resolved);
-  });
+//   let sandbox;
+//   beforeEach(() => {
+//     sandbox = sinon.sandbox.create();
+//     const resolved = new Promise((res) => res({ data: [{ type: 'admin' }] }));
+//     sandbox.stub(axios, 'get').returns(resolved);
+//   });
 
-  afterEach(() => {
-    sandbox.restore();
-  });
+//   afterEach(() => {
+//     sandbox.restore();
+//   });
   
-  it('calls componentWillMount', async function() {
-    const componentWillMountSpy = sinon.spy(App.prototype, 'componentWillMount');
-    const wrapper = mount(<App />);
-    expect(App.prototype.componentWillMount.calledOnce).to.equal(true);
-    componentWillMountSpy.restore();
-  });
+//   it('calls componentWillMount', async function() {
+//     const componentWillMountSpy = sinon.spy(App.prototype, 'componentWillMount');
+//     const wrapper = mount(<App />);
+//     expect(App.prototype.componentWillMount.calledOnce).to.equal(true);
+//     componentWillMountSpy.restore();
+//   });
 
-  it('contains a Navigation bar', function() {
-    const wrapper = mount(<App/>);
-    expect(wrapper.find('Navigation')).to.have.length(1);
-  });
+//   it('contains a Navigation bar', function() {
+//     const wrapper = mount(<App/>);
+//     expect(wrapper.find('Navigation')).to.have.length(1);
+//   });
 
-  it('contains a Routes child component', function() {
-    const wrapper = mount(<App/>);
-    expect(wrapper.find('Routes')).to.have.length(1);
-  });
+//   it('contains a Routes child component', function() {
+//     const wrapper = mount(<App/>);
+//     expect(wrapper.find('Routes')).to.have.length(1);
+//   });
 
-  it('should receive a response from the server', function() {
-    const getUserDataSpy = sinon.spy(users, 'getUserData');
-    expect(getUserDataSpy.called).to.equal(false);
-    const wrapper = mount(<App/>);
-    expect(getUserDataSpy.called).to.equal(true);
-  });
+//   it('should receive a response from the server', function() {
+//     const getUserDataSpy = sinon.spy(users, 'getUserData');
+//     expect(getUserDataSpy.called).to.equal(false);
+//     const wrapper = mount(<App/>);
+//     expect(getUserDataSpy.called).to.equal(true);
+//   });
 
-  it('should detect a user if they are logged in', function() {
-    const wrapper = mount(<App/>);
-    setTimeout(() => {
-      expect(wrapper.state().isLoggedIn).to.equal(true);
-      expect(wrapper.state().isAdmin).to.equal(true);
-    }, 1000);
-  });
+//   it('should detect a user if they are logged in', function() {
+//     const wrapper = mount(<App/>);
+//     setTimeout(() => {
+//       expect(wrapper.state().isLoggedIn).to.equal(true);
+//       expect(wrapper.state().isAdmin).to.equal(true);
+//     }, 1000);
+//   });
 
-  it('should register a login status', async function() {
-    const wrapper = await mount(<App/>);
-    setTimeout(() => {
-      expect(wrapper.state().loginStatus).to.be(boolean);
-    }, 1000);
-  });
+//   it('should register a login status', async function() {
+//     const wrapper = await mount(<App/>);
+//     setTimeout(() => {
+//       expect(wrapper.state().loginStatus).to.be(boolean);
+//     }, 1000);
+//   });
 
-});
+// });
 
