90cb821d9c1081df0da7ec4541d3d2a8aed77def
Jason Chambers <jas.o.chambers@gmail.com>
2017-06-20 20:42:21 -0700

    (refactor) remove outdated tests

--- test/FrontEndTests/Routes-test.js
+++ test/FrontEndTests/Routes-test.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import { expect } from 'chai';
-import Routes from '../../src/public/components/Routes';
+// import React from 'react';
+// import { shallow } from 'enzyme';
+// import { expect } from 'chai';
+// import Routes from '../../src/public/components/Routes';
 
 
-describe('<Routes />', function() {
+// describe('<Routes />', function() {
 
-  const admin = {
-    isLoggedIn: true,
-    isAdmin: true
-  };
+//   const admin = {
+//     isLoggedIn: true,
+//     isAdmin: true
+//   };
 
-  const student = {
-    isLoggedIn: true,
-    isAdmin: false
-  };
+//   const student = {
+//     isLoggedIn: true,
+//     isAdmin: false
+//   };
 
-  const notLoggedIn = {
-    isLoggedIn: false,
-    isAdmin: false
-  };
+//   const notLoggedIn = {
+//     isLoggedIn: false,
+//     isAdmin: false
+//   };
 
-  it('should receive props', () => {
-    const wrapper = shallow(<Routes userPrivs={admin}/>);
-    expect(wrapper.instance().props.userPrivs.isLoggedIn).to.equal(true);
-    expect(wrapper.instance().props.userPrivs.isAdmin).to.equal(true);
-  });
+//   it('should receive props', () => {
+//     const wrapper = shallow(<Routes userPrivs={admin}/>);
+//     expect(wrapper.instance().props.userPrivs.isLoggedIn).to.equal(true);
+//     expect(wrapper.instance().props.userPrivs.isAdmin).to.equal(true);
+//   });
 
-});
+// });
